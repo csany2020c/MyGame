814d952761d76730b768578311126937a9dbe07b
zsuppanvagyok <Zsuppanflorian11>
2021-09-10 12:37:15 +0200

    asd

---
+++ HelloWorld/rigodonat.py
@@ -0,0 +1,68 @@
+import game
+
+class MarioActor(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__('fox.png')
+    def act(self, delta_time: float):
+        super().act(delta_time)
+        self.x += delta_time * 100
+
+
+class MarioStage(game.scene2d.MyStage):
+
+    def create(self):
+        super().create()
+
+        a = MarioActor
+        a.y = 80
+        self.add_actor(a)
+
+        b = MarioActor
+        b.r = 45
+        self.add_actor(b)
+
+
+
+class MasikScr(game.scene2d.MyScreen):
+
+
+    def create(self):
+        super().create()
+        self.r = 100
+        self.g = 0
+        self.b = 150
+
+    def act(self, delta_time: float):
+        super().act(delta_time)
+        if self.elapsed_time > 1:
+            self.game.screen = MarioScr()
+
+class MarioScr(game.scene2d.MyScreen):
+
+
+    def create(self):
+        super().create()
+        self.r = 100
+        self.g = 0
+        self.b = 100
+        self.addStage(MarioStage())
+
+    def act(self, delta_time: float):
+        super().act(delta_time)
+        if self.elapsed_time > 1:
+            self.game.screen = MasikScr()
+
+
+
+class Mario(game.scene2d.MyGame):
+
+
+
+    def create(self):
+        super().create()
+        self.screen = MasikScr()
+
+
+
+
+Mario()
