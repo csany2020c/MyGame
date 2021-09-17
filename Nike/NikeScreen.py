947fca9c3ee2050c6273293c7ed22d98b83a19f2
zsuppanvagyok <Zsuppanflorian11>
2021-09-17 12:35:12 +0200

    screen

--- Nike/NikeScreen.py
+++ Nike/NikeScreen.py
@@ -5,7 +5,7 @@ class NikeScreen(game.scene2d.MyScreen):
 
     def __init__(self):
         super().__init__()
-        self.set_background_color(200, 100, 22)
+        self.set_background_color(100, 0, 100)
         self.add_stage(NikeStage())
 
 
