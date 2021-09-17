71df83637d04f59f77015801d93e86d3d831d1ec
tuskeb <tuskebganz@gmail.com>
2021-09-17 11:34:04 +0200

    Space szétszedése, run() metódus a Game osztályba

---
+++ Space/SpaceGame.py
@@ -0,0 +1,11 @@
+import game
+from GameScreen import *
+
+
+class Space(game.scene2d.MyGame):
+
+    def __init__(self):
+        super().__init__(1280, 720)
+        self.set_screen(GameScreen())
+        self.run()
+
