c23423d275ba93393a8a13e92f9bf7b475c29b29
zsuppanvagyok <Zsuppanflorian11>
2021-09-17 12:17:59 +0200

    screen

---
+++ Nike/NikeGame.py
@@ -0,0 +1,11 @@
+import game
+from NikeScreen import *
+
+
+class Nike(game.scene2d.MyGame):
+
+    def __init__(self):
+        super().__init__(1280, 720)
+        self.set_screen(NikeScreen())
+        self.run()
+
