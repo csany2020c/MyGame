6fba2632c956fb254a76c7ee77e80462145d3000
Da-WEED <zsebokdavidf@gmail.com>
2021-09-20 14:18:57 +0200

    kancsalmate27egycigogipodzsigolo

--- Kancsalmate27megilyenek/MenuGame.py
+++ Kancsalmate27megilyenek/MenuGame.py
@@ -4,9 +4,12 @@ import game
 
 class MainGame(game.scene2d.MyGame):
 
-    def __init__(self, width: int = 1280, height: int = 720):
-        super().__init__(width, height)
+    def __init__(self, width: int = 1280, height: int = 720, autorun: bool = False):
+        super().__init__(width, height, autorun)
         self.screen = MenuScreen3()
+        self.run()
+
+
 
 
 MainGame()
--- Kancsalmate27megilyenek/MenuStage.py
+++ Kancsalmate27megilyenek/MenuStage.py
@@ -1,10 +1,5 @@
-import game
 from Kancsalmate27megilyenek.MenuActor import *
-
-class Actor1(game.scene2d.MyActor):
-    def __init__(self):
-        super().__init__("../HelloWorld/fox.png")
-
+import game
 
 class Stage3(game.scene2d.MyStage):
     def __init__(self):
@@ -12,3 +7,5 @@ class Stage3(game.scene2d.MyStage):
         a = MenuActor1()
         a.y = 50
         self.add_actor(a)
+
+
