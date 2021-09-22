231ab93b585efa51377dd52fab6078047c1014ba
kistapir1 <kistapir0@gmail.com>
2021-09-22 14:18:32 +0200

    fejlesztes2.0

--- HawkProductions/jatek.py
+++ HawkProductions/jatek.py
@@ -6,6 +6,13 @@ class Deagle(game.scene2d.MyActor):
     def __init__(self):
         super().__init__("image/bid.jpg")
 
+class Pile1(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("image/vegre.png")
+
+class Pile2(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("image/vegre.png")
 
 class Stage(game.scene2d.MyStage):
 
@@ -16,6 +23,14 @@ class Stage(game.scene2d.MyStage):
         self.D.y = 100
         self.D.x = 50
         self.D.width = 100
+
+        self.P1 = Pile1()
+        self.add_actor(self.P1)
+
+        self.P2 = Pile2()
+        self.add_actor(self.P2)
+        self.P2.set_x(250)
+        self.P2.set_y(150)
 
 
 class Screen(game.scene2d.MyScreen):
