15432779655fb22f9e0989ea5ba77df82a2c733e
kistapir1 <kistapir0@gmail.com>
2021-09-29 12:40:19 +0200

    asd

--- bruhmomento/bruhStage.py
+++ bruhmomento/bruhStage.py
@@ -8,6 +8,6 @@ class bruhstage(game.scene2d.MyStage):
     def __init__(self):
         super().__init__()
         self.add_actor(bruhActor())
-        self.add_actor(enemy1())
-        self.add_actor(horthy())
         self.add_actor(enemy2())
+        self.add_actor(horthy())
+        self.add_actor(enemy1())
