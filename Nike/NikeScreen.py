5b117fe80bd10f390ed937e12a9ab60fbf00f633
milaan1337 <fellnermilan@gmail.com>
2021-09-29 12:28:23 +0200

    már kék

--- Nike/NikeScreen.py
+++ Nike/NikeScreen.py
@@ -5,7 +5,7 @@ class NikeScreen(game.scene2d.MyScreen):
 
     def __init__(self):
         super().__init__()
-        self.set_background_color(0, 150, 0)
+        self.set_background_color(0, 0, 150)
         self.add_stage(NikeStage())
 
 
