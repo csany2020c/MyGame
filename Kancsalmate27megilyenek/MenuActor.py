9ae841851c16eb4dae803a88dd8fbf43f6ba0940
kistapir1 <kistapir0@gmail.com>
2021-09-29 13:26:20 +0200

    Na jó idiot jani

--- Kancsalmate27megilyenek/MenuActor.py
+++ Kancsalmate27megilyenek/MenuActor.py
@@ -1,5 +1,6 @@
 import game
 
+
 class MenuActor1(game.scene2d.MyActor):
     def __init__(self, image_url: str = ""):
-        super().__init__("Texture/Exitbutton.png")
+        super().__init__("Exitbutton.png")
