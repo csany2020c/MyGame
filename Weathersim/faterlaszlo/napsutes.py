b2f443df3a4a7705567d7f2f53837fecd1151dc1
Fatér László <faterlaszlo@gmail.com>
2021-10-04 18:07:29 +0200

    feladat folytatása

--- Weathersim/faterlaszlo/napsutes.py
+++ Weathersim/faterlaszlo/napsutes.py
@@ -1,6 +1,7 @@
 import game
+import pygame
 
-class napsutesesactor(game.scene2d.MyActor):
+class hatter_actor(game.scene2d.MyActor):
     def __init__(self):
         super().__init__("!_resources/images/landscape.png")
 
@@ -16,36 +17,43 @@ class napocska(game.scene2d.MyActor):
     def act(self, delta_time: float):
         super().act(delta_time)
         self.r += 10 * delta_time
-        self.x += 2 + delta_time
+        self.x += 1.5 + delta_time
 
 
-class napstage(game.scene2d.MyStage):
+class f_stage(game.scene2d.MyStage):
     def __init__(self):
         super().__init__()
-        self.bg = napsutesesactor()
+        self.bg = hatter_actor()
         self.sun = napocska()
         self.add_actor(self.bg)
         self.add_actor(self.sun)
+        self.sun.set_on_key_down_listener(self.key_down)
         self.sun.x = 5
-        self.sun.y = 50
-        self.sun.w = 300
+        self.sun.y = 80
+        self.sun.w = 350
 
 
 
+    def key_down(self, event, sender):
+        print(sender)
+        print(event)
+        if event.key == pygame.K_ESCAPE:
+            print("Sikeresen be lett zárva")
+            pygame.quit()
 
 
-
-class napscreen(game.scene2d.MyScreen):
+class f_screen(game.scene2d.MyScreen):
     def __init__(self):
         super().__init__()
         self.set_background_color(120, 100, 90)
-        self.add_stage(napstage())
+        self.add_stage(f_stage())
 
-class maga_a_game(game.scene2d.MyGame):
+
+class f_game(game.scene2d.MyGame):
     def __init__(self):
         super().__init__()
-        self.set_screen(napscreen())
+        self.set_screen(f_screen())
 
 
-maga_a_game().run()
+f_game().run()
 
