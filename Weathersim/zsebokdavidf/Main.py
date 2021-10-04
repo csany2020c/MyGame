7ee2dc257bc9dc46b208cc38547f69cbf6ddc182
DaW33D <zsebokdavidf@gmail.com>
2021-10-04 22:39:08 +0200

    dolgozatosdi

--- Weathersim/zsebokdavidf/Main.py
+++ Weathersim/zsebokdavidf/Main.py
@@ -1,5 +1,4 @@
-import game
-from Weathersim.zsebokdavidf.MainScreen import MainScreen
+from Weathersim.zsebokdavidf.MainScreen import *
 
 
 class Main(game.scene2d.MyGame):
@@ -7,6 +6,8 @@ class Main(game.scene2d.MyGame):
     def __init__(self, width: int = 1280, height: int = 720, autorun: bool = False, autosize: bool = False):
         super().__init__(width, height, autorun, autosize)
         self.screen = MainScreen()
+
+
 
 
 Main().run()
--- Weathersim/zsebokdavidf/MainScreen.py
+++ Weathersim/zsebokdavidf/MainScreen.py
@@ -1,10 +1,17 @@
-import game
-from Weathersim.zsebokdavidf.MainStage import MainStage
+from Weathersim.zsebokdavidf.MainStage import *
+from Weathersim.zsebokdavidf.RainStage import *
+from Weathersim.zsebokdavidf.SnowStage import *
 
 
 class MainScreen(game.scene2d.MyScreen):
 
     def __init__(self):
         super().__init__()
-        self.set_background_color(r=0, b=255, g=50)
-        self.add_stage(MainStage())
+        self.set_background_color(r=0, b=50, g=0)
+        """self.add_stage(MainStage())"""
+        """self.add_stage(RainStage())"""
+        self.add_stage(SnowStage())
+
+
+
+
