8218e6b896496a8e3d308302d47176e4caa3cb2c
tanulo <doramarton20@gmail.com>
2021-10-04 14:20:15 +0200

    2

--- Weathersim/nemethcsababence/napsutes.py
+++ Weathersim/nemethcsababence/napsutes.py
@@ -19,6 +19,12 @@ class nap(game.scene2d.MyActor):
     def __init__(self):
         self.map = super().__init__('!_resources/images/sun.png')
 
+    def act(self, delta_time: float):
+        super().act(delta_time)
+        if self.x + self.width < game.scene2d.MyGame.get_screen_width():
+            self.x += delta_time * 60
+            self.rotate_with(delta_time * 50)
+
 
 class GameStage(game.scene2d.MyStage):
 
@@ -33,10 +39,10 @@ class GameStage(game.scene2d.MyStage):
         self.eg.height = 1300
         self.eg.z_index = 1
         self.nap = nap()
-        self.nap.width = 600
-        self.nap.height = 600
-        self.nap.y = -150
-        self.nap.x = 500
+        self.nap.width = 500
+        self.nap.height = 500
+        self.nap.y = -130
+        self.nap.x = 0
         self.nap.z_index = 2
         self.add_actor(self.taj)
         self.add_actor(self.eg)
@@ -55,11 +61,11 @@ class GameScreen(game.scene2d.MyScreen):
         self.add_stage(GameStage())
 
 
-class Program(game.scene2d.MyGame):
+class napsutes(game.scene2d.MyGame):
 
     def __init__(self, width: int = 1280, height: int = 720, autorun: bool = False, autosize: bool = False):
         super().__init__(width, height, autorun, autosize)
         self.screen = GameScreen()
 
 
-Program().run()
+napsutes().run()
