4566e1186bf4491ce3a04a7afef9b81f2bb5b725
vizdakmate <mate.vizdak@gmail.com>
2021-10-05 17:40:20 +0200

    doga

--- Weathersim/vizdakmate/Actor.py
+++ Weathersim/vizdakmate/Actor.py
@@ -1,18 +1,13 @@
 import game
 
-class SunnyActor(game.scene2d.MyActor):
+class Sun(game.scene2d.MyActor):
     def __init__(self):
         super().__init__("sun.png")
 
-
-
-
-class backgroundActor(game.scene2d.MyActor):
+class Background(game.scene2d.MyActor):
     def __init__(self):
         super().__init__("landscape.png")
 
-
-
-class skyActor(game.scene2d.MyActor):
+class Sunny(game.scene2d.MyActor):
     def __init__(self):
         super().__init__("sunny.png")
