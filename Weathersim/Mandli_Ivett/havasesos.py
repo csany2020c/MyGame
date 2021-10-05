579a59dca26d9e3c3d79ec5f8935ab5e51e51af3
mandliivett <mandliivett@gmail.com>
2021-10-05 16:08:21 +0200

    asd

---
+++ Weathersim/Mandli_Ivett/havasesos.py
@@ -0,0 +1,52 @@
+import game
+from random import Random
+
+r = Random()
+
+
+class ActorA(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("cloudy.png")
+
+
+class ActorB(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("landscape.png")
+
+
+class ActorC(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("snow.png")
+
+
+class ActorD(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("rain.png")
+
+
+class Stage(game.scene2d.MyStage):
+    def __init__(self):
+        super().__init__()
+        self.a = ActorA()
+        self.b = ActorB()
+        self.c = ActorC()
+        self.d = ActorD()
+        self.add_actor(self.a)
+        self.add_actor(self.b)
+        self.add_actor(self.c)
+        self.add_actor(self.d)
+
+
+class Screen(game.scene2d.MyScreen):
+    def __init__(self):
+        super().__init__()
+        self.add_stage(Stage())
+
+
+class Start(game.scene2d.MyGame):
+    def __init__(self, width: int = 1280, height: int = 720, autorun: bool = False):
+        super().__init__(width, height, autorun)
+        self.screen = Screen()
+
+
+Start().run()
---
+++ Weathersim/Mandli_Ivett/napsuteses.py
@@ -0,0 +1,43 @@
+import game
+
+
+class ActorA(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("sunny.png")
+
+
+class ActorB(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("sun.png")
+
+
+class ActorC(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("landscape.png")
+
+
+class Stage(game.scene2d.MyStage):
+    def __init__(self):
+        super().__init__()
+        self.a = ActorA()
+        self.b = ActorB()
+        self.c = ActorC()
+        self.b.set_x(400).set_y(50)
+        self.add_actor(self.a)
+        self.add_actor(self.b)
+        self.add_actor(self.c)
+
+
+class Screen(game.scene2d.MyScreen):
+    def __init__(self):
+        super().__init__()
+        self.add_stage(Stage())
+
+
+class Start(game.scene2d.MyGame):
+    def __init__(self, width: int = 1280, height: int = 720, autorun: bool = False):
+        super().__init__(width, height, autorun)
+        self.screen = Screen()
+
+
+Start().run()
