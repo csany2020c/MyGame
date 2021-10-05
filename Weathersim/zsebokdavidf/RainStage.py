82cc540a2dc5885cc1508eb640f8621d2d24adf3
daweed <zsebokdavidf@gmail.com>
2021-10-05 23:26:34 +0200

    beadando

--- Weathersim/zsebokdavidf/RainStage.py
+++ Weathersim/zsebokdavidf/RainStage.py
@@ -1,5 +1,3 @@
-import game
-import pygame
 from Weathersim.zsebokdavidf.MainActors import *
 
 
@@ -7,5 +5,14 @@ class RainStage(game.scene2d.MyStage):
 
     def __init__(self):
         super().__init__()
+
+        for k in range(40):
+            self.rain = Rain()
+            size = random.randint(a=20, b=50)
+            self.rain.height = size
+            self.rain.width = size
+            self.rain.x = random.randint(a=0, b=1280)
+            self.rain.y = random.randint(a=-0, b=720)
+            self.add_actor(self.rain)
 
         self.add_actor(Cloud())
