966f95b621b6f74c6ff5e8dd86ce528d1cb5188d
kalamarrajmund <rajmundka1@gmail.com>
2021-10-06 09:33:42 +0200

    dettogeriasz

--- fujpacal/aknamenu.py
+++ fujpacal/aknamenu.py
@@ -1,3 +1,12 @@
 import game
-import pygame
+
+
+class Menu()
+
+    def set_screen_game(self):
+        self.set_screen_game(GameScreen)
+
+
+    def set_screen_menu(self):
+        self.set_screen_menu(GameScreen)
 
