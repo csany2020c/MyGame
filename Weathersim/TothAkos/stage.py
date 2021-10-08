5cb6c5c51e3de439901febc14c4025b9ac69fe93
tothakos9c <ketyor06@gmail.com>
2021-10-08 08:57:19 +0200

    abc

--- Weathersim/TothAkos/stage.py
+++ Weathersim/TothAkos/stage.py
@@ -1,9 +1,4 @@
-import game
 from Weathersim.TothAkos.actor import *
-from game.scene2d import MyTickTimer
-import random
-import pygame
-
 
 class Sunnystage(game.scene2d.MyStage):
     def __init__(self):
@@ -13,18 +8,12 @@ class Sunnystage(game.scene2d.MyStage):
         self.add_actor(Landscape())
         self.Sun = Sun()
         self.add_actor(self.Sun)
-        for j in range(1):
-            for i in range(1):
-                e = Sun()
-                e.y = i * 40
-                e.x = j * 40
-                # e.width = 10
-                # e.height = 10
-                self.add_actor(e)
 
-    def key_down(self, sender, event):
-        print(sender)
-        print(event)
-        if event.key == pygame.K_ESCAPE:
-            print("'QUIT'")
-            quit()
+class Snowystage (game.scene2d.MyStage):
+    def __init__(self):
+        super().__init__()
+        self.b = Snow
+        self.add_actor(cloudy)
+        self.add_actor(Landscape)
+        self.Snow = Snow()
+        self.add_actor(self.Snow)
