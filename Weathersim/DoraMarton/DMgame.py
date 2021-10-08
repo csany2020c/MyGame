3f683c0e5f64766185511f6b0ca7b886d28a75cd
Win10 <doramarton20@gmail.com>
2021-10-08 10:03:10 +0200

    majnemkesz:))

--- Weathersim/DoraMarton/DMgame.py
+++ Weathersim/DoraMarton/DMgame.py
@@ -5,7 +5,7 @@ class main(game.scene2d.MyGame):
 
     def __init__(self):
         super().__init__()
-        self.screen = sunnyscreen()
+        self.screen = Sunnyscreen()
         self.run()
 
 main()
