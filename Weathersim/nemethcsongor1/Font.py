73aaef09d1bb541981b3edf96993ac47d94aa6f6
BYAX12 <csongor.nemeth05@gmail.com>
2021-10-16 22:39:04 +0100

    Kész WeatherSim UI.:Még nem telt le a három nap, ugye?

--- Weathersim/nemethcsongor1/Font.py
+++ Weathersim/nemethcsongor1/Font.py
@@ -11,7 +11,15 @@ class Font(game.scene2d.MyLabel):
 
 class Font2(game.scene2d.MyLabel):
     def __init__(self, string: str = "MyText") -> None:
-        game.scene2d.MyLabel.__init__(self, string=string, font_name="Cantara")
+        game.scene2d.MyLabel.__init__(self, string=string, font_name="fonts/Rockabye-Regular.otf")
+
+    def act(self, delta_time: float):
+        game.scene2d.MyLabel.act(self, delta_time)
+
+
+class Font3(game.scene2d.MyLabel):
+    def __init__(self, string: str = "MyText") -> None:
+        game.scene2d.MyLabel.__init__(self, string=string, font_name="fonts/OldLondon.ttf")
 
     def act(self, delta_time: float):
         game.scene2d.MyLabel.act(self, delta_time)
