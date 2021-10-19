a0fa2a54c6df4a213d549be4aaa6f53371888215
kistapir1 <kistapir0@gmail.com>
2021-10-19 13:30:22 +0200

    sziamizu

--- bruhmomento/bruhActor.py
+++ bruhmomento/bruhActor.py
@@ -1,25 +1,29 @@
 import pygame
 import game
 import pygame
+from game.simpleworld.ShapeType import ShapeType
 
-class bruhActor(game.scene2d.MyActor):
+class fohos(game.scene2d.MyActor):
     def __init__(self):
         super().__init__("Images/legokatona.png")
-
-        def key_down(sender, event):
-            print(sender)
-            print(event)
-            if event.key == pygame.K_d:
-                self.x += 4
-            if event.key == pygame.K_w:
-                self.y -= 4
-            if event.key == pygame.K_a:
-                self.x -= 4
-            if event.key == pygame.K_s:
-                self.y += 4
-
-        self.set_on_key_press_listener(key_down)
+        self.set_on_key_press_listener(self.key_down)
         self.set_size(200, 100)
+        self.hitbox_scale_h = 0.9
+        self.hitbox_scale_w = 0.9
+        # self.hitbox_shape = ShapeType.Rectangle
+
+    def key_down(self, sender, event):
+        print(sender)
+        print(event)
+        if event.key == pygame.K_d:
+            self.x += 4
+        if event.key == pygame.K_w:
+            self.y -= 4
+        if event.key == pygame.K_a:
+            self.x -= 4
+        if event.key == pygame.K_s:
+            self.y += 4
+
 class enemy1(game.scene2d.MyActor):
     def __init__(self):
         super().__init__("Images/katona.png")
@@ -36,10 +40,13 @@ class enemy2 (game.scene2d.MyActor):
         super().act(delta_time)
         self.x += delta_time * 100
 
-    def act(self, delta_time: float):
-        super().act(delta_time)
-        self.x += delta_time * 100
-
 class map(game.scene2d.MyActor):
     def __init__(self):
         super().__init__("Images/map.jpg")
+
+class kapu(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("Images/kkapu.png")
+        self.hitbox_scale_h = 0.9
+        self.hitbox_scale_w = 0.9
+        # self.hitbox_shape = ShapeType.Rectangle
