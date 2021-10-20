20e7698e785b0a49e0f2ae5bb9486838f198cfcc
BYAX12 <csongor.nemeth05@gmail.com>
2021-10-20 08:19:13 +0200

    elnevezett képek HP

--- HawkProductions/Actors.py
+++ HawkProductions/Actors.py
@@ -3,7 +3,7 @@ import game
 
 class Sellect(game.scene2d.MyActor):
     def __init__(self):
-        super().__init__("image/Select.png")
+        super().__init__("image/sellect.jpg")
 
 
 class Title(game.scene2d.MyActor):
