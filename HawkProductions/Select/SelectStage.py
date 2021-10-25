b59330f1fdd0b2e3a217be90f18dcd5953e5e4f5
Fatér László <faterlaszlo@gmail.com>
2021-10-25 16:45:48 +0200

    Átnevzés

--- HawkProductions/Select/SelectStage.py
+++ HawkProductions/Select/SelectStage.py
@@ -4,6 +4,7 @@ from HawkProductions.Font import *
 import HawkProductions.Game.GameScreen
 import HawkProductions.Game2.GameScreen2
 import HawkProductions.Music
+import pygame
 
 
 class SelectStgage(game.scene2d.MyStage):
@@ -14,7 +15,7 @@ class SelectStgage(game.scene2d.MyStage):
         pygame.mixer.music.play(-1)
         self.t = Arrow()
         self.add_actor(self.t)
-        self.t.set_on_mouse_down_listener(self.click)
+        self.t.set_on_mouse_down_listener(self.click_m)
         self.t.w = 125
 
         self.D = Deagle1()
@@ -23,38 +24,38 @@ class SelectStgage(game.scene2d.MyStage):
         self.D.y = 250
         self.D.width = 200
 
-        self.t1 = Sellect()
-        self.add_actor(self.t1)
-        self.t1.width = 200
-        self.t1.x = 530
-        self.t1.y = 400
-        self.t1.set_on_mouse_down_listener(self.click1)
+        self.s1 = Sellect()
+        self.add_actor(self.s1)
+        self.s1.width = 200
+        self.s1.x = 530
+        self.s1.y = 400
+        self.s1.set_on_mouse_down_listener(self.click_g1)
 
-        self.arrow = Arrow()
-        self.add_actor(self.arrow)
-        self.arrow.x = 330
-        self.arrow.y = 280
-        self.arrow.w = 200
-        self.arrow.set_on_mouse_down_listener(self.click2)
+        self.arrow_l1 = Arrow()
+        self.add_actor(self.arrow_l1)
+        self.arrow_l1.x = 330
+        self.arrow_l1.y = 280
+        self.arrow_l1.w = 200
+        self.arrow_l1.set_on_mouse_down_listener(self.click_l1)
 
-        self.arrow3 = Arrow_r()
-        self.add_actor(self.arrow3)
-        self.arrow3.x = 730
-        self.arrow3.y = 280
-        self.arrow3.w = 200
-        self.arrow3.set_on_mouse_down_listener(self.click3)
+        self.arrow_r1 = Arrow_r()
+        self.add_actor(self.arrow_r1)
+        self.arrow_r1.x = 730
+        self.arrow_r1.y = 280
+        self.arrow_r1.w = 200
+        self.arrow_r1.set_on_mouse_down_listener(self.click_r1)
 
-        self.arrow1 = Arrow()
-        self.arrow1.x = 330
-        self.arrow1.y = 280
-        self.arrow1.w = 200
-        self.arrow1.set_on_mouse_down_listener(self.click4)
+        self.arrow_l2 = Arrow()
+        self.arrow_l2.x = 330
+        self.arrow_l2.y = 280
+        self.arrow_l2.w = 200
+        self.arrow_l2.set_on_mouse_down_listener(self.click4)
 
-        self.arrow2 = Arrow_r()
-        self.arrow2.x = 730
-        self.arrow2.y = 280
-        self.arrow2.w = 200
-        self.arrow2.set_on_mouse_down_listener(self.click5)
+        self.arrow_r2 = Arrow_r()
+        self.arrow_r2.x = 730
+        self.arrow_r2.y = 280
+        self.arrow_r2.w = 200
+        self.arrow_r2.set_on_mouse_down_listener(self.click5)
 
         self.arrow4 = Arrow()
         self.arrow4.x = 330
@@ -71,65 +72,80 @@ class SelectStgage(game.scene2d.MyStage):
         self.D2.y = 280
         self.D2.w = 200
 
-        self.t2 = Sellect()
-        self.t2.width = 200
-        self.t2.x = 530
-        self.t2.y = 400
-        self.t2.set_on_mouse_down_listener(self.clickk1)
+        self.s2 = Sellect()
+        self.s2.width = 200
+        self.s2.x = 630
+        self.s2.y = 400
+        self.s2.set_on_mouse_down_listener(self.click_g2)
 
         self.D3 = Deagle3()
         self.D3.x = 520
         self.D3.y = 280
         self.D3.w = 200
 
-    def click(self, sender, event):
+        self.s3 = Sellect()
+        self.s3.width = 200
+        self.s3.x = 630
+        self.s3.y = 400
+        self.s3.set_on_mouse_down_listener(self.click_g3)
+
+    def click_m(self, sender, event):
         if event.button == 1:
             self.screen.game.set_screen(HawkProductions.menu.MenuScreen.MenuScreen())
 
-    def click1(self, sender, event):
+    def click_g1(self, sender, event):
         if event.button == 1:
             self.screen.game.set_screen(HawkProductions.Game.GameScreen.GameScreen())
 
-    def clickk1(self, sender, event):
+    def click_g2(self, sender, event):
         if event.button == 1:
             self.screen.game.set_screen(HawkProductions.Game2.GameScreen2.GameScreen())
 
-    def click2(self, sender, event):
+    def click_g3(self, sender, event):
+        if event.button == 1:
+            self.screen.game.set_screen(HawkProductions.Game.GameScreen.GameScreen())
+
+    def click_l1(self, sender, event):
         if event.button == 1:
             self.remove_actor(self.D)
             self.add_actor(self.D2)
-            self.remove_actor(self.arrow)
-            self.add_actor(self.arrow1)
-            self.add_actor(self.arrow2)
-            self.remove_actor(self.arrow3)
-            self.remove_actor(self.t1)
-            self.add_actor(self.t2)
+            self.remove_actor(self.arrow_l1)
+            self.add_actor(self.arrow_l2)
+            self.add_actor(self.arrow_r2)
+            self.remove_actor(self.arrow_r1)
+            self.remove_actor(self.s1)
+            self.add_actor(self.s2)
 
-    def click3(self, sender, event):
+
+    def click_r1(self, sender, event):
         if event.button == 1:
             self.remove_actor(self.D)
             self.add_actor(self.D3)
-            self.remove_actor(self.arrow)
+            self.remove_actor(self.arrow_l1)
             self.add_actor(self.arrow4)
-            self.remove_actor(self.arrow3)
+            self.remove_actor(self.arrow_r1)
             self.add_actor(self.arrow5)
+            self.remove_actor(self.s1)
+            self.add_actor(self.s2)
 
     def click4(self, sender, event):
         if event.button == 1:
             self.remove_actor(self.D2)
             self.add_actor(self.D3)
-            self.remove_actor(self.arrow1)
+            self.remove_actor(self.arrow_l2)
             self.add_actor(self.arrow4)
-            self.remove_actor(self.arrow2)
+            self.remove_actor(self.arrow_r2)
             self.add_actor(self.arrow5)
+            self.remove_actor(self.s1)
+            self.remove_actor(self.s2)
 
     def click5(self, sender, event):
         if event.button == 1:
             self.remove_actor(self.D2)
             self.add_actor(self.D)
-            self.remove_actor(self.arrow1)
-            self.add_actor(self.arrow)
-            self.remove_actor(self.arrow2)
-            self.add_actor(self.arrow3)
-            self.remove_actor(self.t2)
-            self.add_actor(self.t1)
+            self.remove_actor(self.arrow_l2)
+            self.add_actor(self.arrow_l1)
+            self.remove_actor(self.arrow_r2)
+            self.add_actor(self.arrow_r1)
+            self.add_actor(self.s1)
+            self.remove_actor(self.s2)
