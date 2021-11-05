90ccfcc3fcba244934b7ae3c965209523060d854
milaan1337 <fellnermilan@gmail.com>
2021-11-05 08:46:57 +0100

    123

---
+++ File/1_feladat/TothAkos.py
@@ -0,0 +1,35 @@
+import string
+from typing import TextIO
+from typing import List
+
+class Data:
+
+    def __init__(self, parseString: str) -> None:
+        super().__init__()
+        print("Create Data from String")
+        print(parseString)
+        fields: List['str'] = parseString.split(" ")
+        self.x: int = int(fields[0])
+        self.y: int = int(fields[1])
+        self.color: int = (int(fields[2]), int(fields[3]), int(fields[4]))
+        self.text: str = ""
+        for i in range(5, len(fields)):
+            self.text += str(fields[i])
+            if i < len(fields) - 1:
+                self.text += " "
+
+    def __str__(self) -> str:
+        return "x = {x}; y = {y}; text = {txt}; color = {col}".format(x=self.x, y=self.y, txt=self.text, col = self.color)
+
+
+class Main:
+    def __init__(self) -> None:
+        super().__init__()
+        f: TextIO = open("!_Spec/orvosi_nobeldijak.txt")
+        content: str = f.read()
+        print("Content:")
+        print(content)
+        lines: List['str'] = content.split(sep="\n")
+        f.close()
+
+Main()
