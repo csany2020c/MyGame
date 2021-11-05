a8ae90b32f2b290ba7f3968de83e6dda3eb0b4a5
zsuppanvagyok <Zsuppanflorian11>
2021-11-05 08:56:39 +0100

    asd

--- File/1_feladat/rigodonat.py
+++ File/1_feladat/rigodonat.py
@@ -9,17 +9,13 @@ class Data:
         print("Create Data from String")
         print(parseString)
         fields: List['str'] = parseString.split(" ")
-        self.x: int = int(fields[0])
-        self.y: int = int(fields[1])
-        self.color: int = (int(fields[2]), int(fields[3]), int(fields[4]))
+
         self.text: str = ""
-        for i in range(5, len(fields)):
+        for i in range(0, len(fields)):
             self.text += str(fields[i])
             if i < len(fields) - 1:
                 self.text += " "
 
-    def __str__(self) -> str:
-        return "x = {x}; y = {y}; text = {txt}; color = {col}".format(x=self.x, y=self.y, txt=self.text, col=self.color)
 
 class Main:
 
