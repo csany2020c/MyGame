c2d8c8378d71da1417c51412aa07e11a46fbf560
tanulo <rdonat123@gmail.com>
2021-11-09 12:46:59 +0100

    asdsasdasdasdasda

--- File/1_feladat/bobicsbarnabas.py
+++ File/1_feladat/bobicsbarnabas.py
@@ -8,18 +8,19 @@ class Data:
         super().__init__()
         print("Create Data from String")
         print(parseString)
-        fields: List['str'] = parseString.split(" ")
-        self.x: int = int(fields[0])
-        self.y: int = int(fields[1])
-        self.color: int = (int(fields[2]), int(fields[3]), int(fields[4]))
+        fields: List['str'] = parseString.split(";")
         self.text: str = ""
+        self.ev: int = int(fields[0])
+        self.nev: str = fields[1]
+        self.szuleteshalalozas: str = fields[2]
+        self.orszagkod: str = fields[3]
         for i in range(5, len(fields)):
             self.text += str(fields[i])
             if i < len(fields) - 1:
                 self.text += " "
 
     def __str__(self) -> str:
-        return "x = {x}; y = {y}; text = {txt}; color = {col}".format(x=self.x, y=self.y, txt=self.text, col = self.color)
+        return "Ev = {x}; Nev = {y}; Szuleteshalalozas = {txt}; Orszagkod = {col}".format(x=self.ev, y=self.nev, txt=self.szuleteshalalozas, col = self.orszagkod)
 
 
 class Main:
@@ -31,7 +32,17 @@ class Main:
         print("Content:")
         print(content)
         lines: List['str'] = content.split(sep="\n")
-        f.close()
+        print("Split content")
+        print(lines)
+        print("Load to List")
+        datalist: List['Data'] = list()
+        i : int = 0
+        for i in range(1, len(lines) - 1):
+            d = Data(lines[i])
+            datalist.append(d)
+        print("Print list")
+        for d in datalist:
+            print(d)
 
 
 Main()
