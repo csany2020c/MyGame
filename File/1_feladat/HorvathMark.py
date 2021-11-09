58bc18d5a16bc8886db8e833871d651925ea8b3f
Benceplays <nemth.csaba.bence@gmail.com>
2021-11-09 12:44:48 +0100

    DONE

--- File/1_feladat/HorvathMark.py
+++ File/1_feladat/HorvathMark.py
@@ -4,10 +4,8 @@ from typing import TextIO
 class data:
     def __init__(self, parseString: str) -> None:
         super().__init__()
-        print(parseString)
         fields: List['str'] = parseString.split(";")
-        print(fields)
-        self.text: str = fields[0]
+        self.text: int = int(fields[0])
         self.nev: str = fields[1]
         self.elet: str = fields[2]
         self.orszag: str = fields[3]
@@ -22,11 +20,8 @@ class olvasas:
         super().__init__()
         f: TextIO = open("!_Spec/orvosi_nobeldijak.txt", "r")
         content: str = f.read()
-        print(content)
         lines: List['str'] = content.split(sep="\n")
-        print(lines)
         datalist: List['data'] = list()
-        i: int = 0
         for i in range(1, len(lines) - 1):
             d = data(lines[i])
             datalist.append(d)
