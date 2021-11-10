2714da27723501325b834d87015891814f4505cf
tanulo <kancsalmate27@gmail.com>
2021-11-10 09:30:48 +0100

    ZSa buta rajmind

---
+++ File/2_feladat/olahgergo.py
@@ -0,0 +1,32 @@
+from typing import List
+from typing import TextIO
+
+
+class Eloado:
+    def __init__(self, parseString: str) -> None:
+        super().__init__()
+        print(parseString)
+        fields: List['str'] = parseString.split("\t")
+        self.eloadoid: int = int(fields[0])
+        self.helyezes: str = fields[1]
+        self.zenekar: str = fields[2]
+
+    def __str__(self) -> str:
+        return "Előadó = {x}; Helyezés = {y}; Zenekar = {txt}".format(x=self.eloadoid, y=self.helyezes, txt=self.zenekar)
+
+class read:
+    def __init__(self) -> None:
+        super().__init__()
+        f: TextIO = open("!_Specs/eloadok.txt", "r", encoding="utf-8")
+        content: str = f.read()
+        lines: List['str'] = content.split(sep="\n")
+        datalist: List['Eloado'] = list()
+        for s in range(1, len(lines) - 1):
+            c = Eloado(lines[s])
+            datalist.append(c)
+
+        for d in datalist:
+            print(d)
+        f.close()
+
+read()
