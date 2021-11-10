72923399da07401162ce9e03cb97b3eafd1552ac
milaan1337 <fellnermilan@gmail.com>
2021-11-10 08:41:55 +0100

    zsuppanvoltam ujra 99

--- File/1_feladat/zsuppanflorian.py
+++ File/1_feladat/zsuppanflorian.py
@@ -2,29 +2,48 @@ import string
 from typing import TextIO
 from typing import List
 
+
 class Data:
 
     def __init__(self, parseString: str) -> None:
         super().__init__()
         print(parseString)
+
         fields: List['str'] = parseString.split(";")
-        # self.text: str = ""
+        self.year: int = int(fields[0])
+        self.name: str = str(fields[1])
+        self.born: str = str(fields[2])
+        self.country: str = str(fields[3])
+
+        for i in range(5, len(
+                fields)):
+            self.text += str(fields[i])
 
     def __str__(self) -> str:
-        return "valami"
+        return "Year = {x}; Name = {y}; Born-Die = {txt}; Country = {col}".format(x=self.year, y=self.name, txt=self.born, col=self.country)
 
 
 class Main:
 
     def __init__(self) -> None:
         super().__init__()
-        f: TextIO = open("!_Spec/orvosi_nobeldijak.txt", "r")
+        f: TextIO = open("!_Spec/orvosi_nobeldijak.txt", "r", encoding="utf-8")
         content: str = f.read()
+        f.close()
+        print("Content:")
+        print(content)
         lines: List['str'] = content.split(sep="\n")
+        print("Split content")
+        print(lines)
+        print("Load to List")
         datalist: List['Data'] = list()
-        for i in range(1, len(lines) - 1):
+        for i in range(1, len(lines)-1):
             d = Data(lines[i])
             datalist.append(d)
+        print("Print list")
+        for d in datalist:
+            print(d)
         f.close()
+
 
 Main()
