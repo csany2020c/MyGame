275aed7e05f6fd681779942d7f1bbb5123b0e506
zsuppanvagyok <Zsuppanflorian11>
2021-11-12 08:21:54 +0100

    asd

---
+++ File/2_feladat/vizdakmate.py
@@ -0,0 +1,47 @@
+import string
+from typing import TextIO
+from typing import List
+
+class Data:
+
+    def __init__(self, parseString: str) -> None:
+        super().__init__()
+        #print("Create Data from String")
+        #print(parseString)
+        fields: List['str'] = parseString.split(";")
+        #self.text: str = ""
+        self.eloado: int = fields[0]
+        self.nev: str = fields[1]
+        self.zenekar: str = fields[2]
+
+        for i in range(5, len(fields)):
+            self.text += str(fields[i])
+            if i < len(fields) - 1:
+                self.text += " "
+
+
+
+
+class Main:
+
+    def __init__(self) -> None:
+        super().__init__()
+        f: TextIO = open("!_Specs/dalok.txt", "r")
+        content: str = f.read()
+        print("Content:")
+        print(content)
+        lines: List['str'] = content.split(sep="\n")
+        #print("Split content")
+        #print(lines)
+        #print("Load to List")
+        datalist: List['Data'] = list()
+        i : int = 0
+        for i in range(1, len(lines) - 1):
+            d = Data(lines[i])
+            datalist.append(d)
+        print("Print list")
+        for d in datalist:
+            print(d)
+
+
+Main()
