5e55cff1fe013af3d64a000b703e7cee5eb6d220
tanulo <kancsalmate27@gmail.com>
2021-11-16 13:00:32 +0100

    asd

--- File/2_feladat/KisKornel2.py
+++ File/2_feladat/KisKornel2.py
@@ -3,62 +3,77 @@ from typing import TextIO
 from typing import List
 
 
-class Data:
-
-
+class Eloadok:
 
     def __init__(self, parseString: str) -> None:
         super().__init__()
-        print("Create Data from String")
-        print(parseString)
-
-        fields: List['str'] = parseString.split("   ")
-
-        self.dalid: str = fields[1]
+        fields: List['str'] = parseString.split("\t")
+        self.eloado: str = fields[1]
         self.nev: str = fields[2]
-        self.list: str = fields[3]
+        self.zenekar: str = fields[3]
 
 
     def __str__(self) -> str:
 
-        return "x = {x}; y = {y}; text = {txt};".format(x=self.dalid, y=self.nev, txt=self.list)
+        return "x = {x}; y = {y}; text = {txt};".format(x=self.eloado, y=self.nev, txt=self.zenekar)
 
+class dalok:
+
+    def __init__(self, parseString: str) -> None:
+        super().__init__()
+        fields: List['str'] = parseString.split("\t")
+        self.dalid: str = fields[1]
+        self.eloadoid: str = fields[2]
+        self.cim: str = fields[3]
+        self.megjelenes: str = fields[4]
+
+    def __str__(self) -> str:
+        return "x = {x}; y = {y}; a = {a}; b = {b}".format(x=self.dalid, y=self.eloadoid, a=self.cim, b=self.megjelenes)
+
+class lista:
+    def __init__(self, parseString: str) -> None:
+        super().__init__()
+        fields: List['str'] = parseString.split("\t")
+        self.ev: str = fields[1]
+        self.helyezes: str = fields[2]
+        self.dalid: str = fields[3]
+
+
+    def __str__(self) -> str:
+        return "x = {x}; y = {y}; a = {a};".format(x=self.ev, y=self.helyezes, a=self.dalid)
 
 class Main:
     def __init__(self) -> None:
         super().__init__()
-        i: int = 0
-        f: TextIO = open("!_Specs/lista.txt", "r")
+        f: TextIO = open("!_Specs/eloadok.txt", "r", encoding="utf-8")
         content: str = f.read()
-        print("Content:")
-        print(content)
         lines: List['str'] = content.split(sep="\n")
-        print("Split content")
-        print(lines)
-        print("Load to List")
-        datalist: List['Data'] = list()
+        datalist: List['Eloadok'] = list()
         for i in range(1, len(lines) - 1):
-            d = Data(lines[i])
+            d = Eloadok(lines[i])
             datalist.append(d)
-        print("Print list")
         for d in datalist:
             print(d)
 
-        g: TextIO = open("!_Specs/dalok.txt", "r")
-        content: str = g.read()
-        print("Content:")
-        print(content)
-        lines: List['str'] = content.split(sep="/\n")
-        print("Split content")
-        print(lines)
-        print("Load to list")
-        datalist: List['Data'] = list()
+        f: TextIO = open("!_Specs/dalok.txt", "r")
+        content: str = f.read()
+        lines: List['str'] = content.split(sep="\n")
+        datalist: List['dalok'] = list()
         for i in range(1, len(lines) - 1):
-            a = Data(lines[i])
-            datalist.append(a)
-        print("Print list")
-        for a in datalist:
-            print(a)
+            e = dalok(lines[i])
+            datalist.append(e)
+        for e in datalist:
+            print(e)
 
-        f.close()
+        f: TextIO = open("!_Specs/lista.txt", "r")
+        content: str = f.read()
+        lines: List['str'] = content.split(sep="\n")
+        datalist: List['lista'] = list()
+        for i in range(1, len(lines) - 1):
+            x = lista(lines[i])
+            datalist.append(x)
+        for x in datalist:
+            print(x)
+
+
 Main()
