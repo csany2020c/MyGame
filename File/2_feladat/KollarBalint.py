f58d80152348670fe410e3d801ac15919d6ba561
Benceplays <nemth.csaba.bence@gmail.com>
2021-11-16 12:18:51 +0100

    push

--- File/2_feladat/KollarBalint.py
+++ File/2_feladat/KollarBalint.py
@@ -25,11 +25,24 @@ class Data2:
         self.elso: str = fields[0]
         self.masodik: str = fields[1]
         self.harmadik: str = fields[2]
-        self.negyedik: str = fields[3]
 
 
     def __str__(self) -> str:
-        return "d = {a}; e = {b}; f = {c}; d = {d}".format(a=self.elso, b=self.masodik, c=self.harmadik, d=self.negyedik)
+        return "d = {a}; e = {b}; f = {c}".format(a=self.elso, b=self.masodik, c=self.harmadik)
+
+class Data3:
+
+    def __init__(self, parseString: str) -> None:
+        super().__init__()
+        print(parseString)
+        fields: List['str'] = parseString.split("\t")
+        self.elso: str = fields[0]
+        self.masodik: str = fields[1]
+        self.harmadik: str = fields[2]
+
+
+    def __str__(self) -> str:
+        return "g = {a}; h = {b}; i = {c}".format(a=self.elso, b=self.masodik, c=self.harmadik)
 
 class Main:
 
@@ -60,5 +73,19 @@ class Main:
         for d in datalist:
             print(d)
         f.close()
+
+        # Eloadok
+        f: TextIO = open("!_Specs//dalok.txt", "r")
+        content: str = f.read()
+        lines: List['str'] = content.split(sep="\n")
+        datalist: List['Data3'] = list()
+        for i in range(1, len(lines) - 1):
+            d = Data3(lines[i])
+            datalist.append(d)
+
+        for d in datalist:
+            print(d)
+        f.close()
+
 
 Main()
