9e54b8f239ecc02a50840d957a19e242e88aa11c
feketefelix555 <fekete.felix2005@gmail.com>
2021-11-17 09:29:09 +0100

    igen

--- File/1_feladat/marcontamas.py
+++ File/1_feladat/marcontamas.py
@@ -1,15 +1,50 @@
 import string
 from typing import TextIO
 from typing import List
-
 class Data:
 
-    def __init__(self, parseString: str) -> None:
+    def __init__(self, text) -> None:
         super().__init__()
-        print("Create Data from String")
-        print(parseString)
-        fields: List['str'] = parseString.split(" ")
-        self.x: int = int(fields[0])
-        self.y: int = int(fields[1])
-        self.color: int = (int(fields[2]), int(fields[3]), int(fields[4]))
-        self.text: str = ""
+        print(text)
+
+        fields:List['str'] = text.split(";")
+
+        self.year : int = int(fields[0])
+        self.name : str = str(fields[1])
+        self.borthdeath : str = str(fields[2])
+        self.cCode : str = str(fields[3])
+
+
+    def __str__(self) -> str:
+        # Formázott szöveg, str.format(...) . A {} közé rakott szövegek a formati függvényben paraméterként használhatók, érték adható a helyükre.
+        return "year = {y}; name = {n}; borthdeath = {bd}; countryCode = {cc}".format(y=self.year, n=self.name,
+                                                                                      bd=self.borthdeath, cc=self.cCode)
+
+
+class Main:
+    def __init__(self) -> None:
+        super().__init__()
+
+        t = TextIO = open("!_Spec/orvosi_nobeldijak.txt","r")
+        szoveg : str = t.read()
+        t.close()
+        print("tartalom:")
+
+        sorok : List['str'] = szoveg.split(sep="\n")
+        print(sorok)
+
+        dataList:List['Data'] = list()
+
+        for i in range(1, len(sorok) - 1):
+            # Új példány létrehozása, a bemenet egy str. A további daraboládokat a Data konstruktora végzi.
+            d = Data(sorok[i])
+            # Hozzáfűzés a listához.
+            dataList.append(d)
+        print("Print list")
+
+        # Végigjárj a datalist listát, melynek minden eleme Data típusú.
+        for d in dataList:
+            # Mivel fent van egy szépen megírt __str__ függvény, ezlért a Data típus könnyedén, automatikusan (implicit) str típussá alakul.
+            print(d)
+
+Main()
