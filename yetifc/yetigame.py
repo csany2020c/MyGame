86b4f18bd3bcafd083e8258b97f28044c590e622
Da-WEED <zsebokdavidf@gmail.com>
2021-11-17 09:20:34 +0100

    zsupp fajlpk

---
+++ yetifc/yetigame.py
@@ -0,0 +1,18 @@
+from yetifc.yetiscreen import *
+from yetifc import *
+import game
+
+
+class Yeti(game.scene2d.MyGame):
+
+    def __init__(self, width: int = 1280, height: int = 720, autorun: bool = False, autosize: bool = False):
+        super().__init__(width, height, autorun, autosize)
+        self.screen = MenuScreen()
+        self.set_on_key_down_listener(self.key_down)
+
+    def key_down(self, sender, event):
+        print(sender)
+        print(event)
+
+
+Yeti().run()
