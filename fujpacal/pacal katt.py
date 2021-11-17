1e486dddb1cb89508d8a8a285e310a745ab74510
kalamarrajmund <rajmundka1@gmail.com>
2021-11-17 09:24:49 +0100

    dettogeriasz

--- fujpacal/pacal katt.py
+++ fujpacal/pacal katt.py
@@ -30,7 +30,7 @@ class PacalScreen(game.scene2d.MyScreen):
 
     def __init__(self):
         super().__init__()
-        self.set_background_color(0, 0, 0)
+        self.set_background_color(0, 1, 0)
         self.add_stage(PacalStage())
 
 
