3fe6374f91eed22461ff09423041f5c288b23803
CrockRock <danielekler@gmail.com>
2021-11-20 18:24:53 +0100

    A game overbe bele tettem egy ismerős nótát és az infóban is megjavítottam a zenét És a laci valami fekete mágiával egy rendesen generálódó akadályokat hozott létre

--- HawkProductions/over/OverStage.py
+++ HawkProductions/over/OverStage.py
@@ -4,6 +4,9 @@ import pygame
 class OverStage(game.scene2d.MyStage):
     def __init__(self):
         super().__init__()
+        pygame.mixer.init()
+        pygame.mixer.music.load("../HawkProductions/Music/Over.wav")
+        pygame.mixer.music.play(-1)
         self.F = Arial()
         self.add_actor(self.F)
         self.F.set_text("Game Over")
