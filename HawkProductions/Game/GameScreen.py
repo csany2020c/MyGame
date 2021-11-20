c2367b93fc8f7f44becd8ed0fca07119c95bbc65
Fatér László <faterlaszlo@gmail.com>
2021-11-20 17:52:02 +0100

    Jatek fejlesztés

--- HawkProductions/Game/GameScreen.py
+++ HawkProductions/Game/GameScreen.py
@@ -1,8 +1,8 @@
-from HawkProductions.Game.GameStage import *
+from HawkProductions.Game.GameStage1 import *
 
 
 class GameScreen(game.scene2d.MyScreen):
 
     def __init__(self):
         super().__init__()
-        self.add_stage(GameStage())
+        self.add_stage(GameStage1())
