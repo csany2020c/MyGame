111132ef17578d58e9d8b7315b6760d3a919ff55
Fatér László <faterlaszlo@gmail.com>
2021-11-20 18:07:58 +0100

    Jatek fejlesztés

--- HawkProductions/over/OverStage.py
+++ HawkProductions/over/OverStage.py
@@ -1,4 +1,7 @@
+import game
+import pygame
 from HawkProductions.Font import *
+import HawkProductions.menu.MenuScreen
 
 
 class OverStage(game.scene2d.MyStage):
@@ -8,3 +11,19 @@ class OverStage(game.scene2d.MyStage):
         self.add_actor(self.F)
         self.F.set_text("Game Over")
         self.F.set_color(204, 0, 0)
+        self.F.x = 500
+        self.F.y = 250
+
+        self.g = Arial()
+        self.add_actor(self.g)
+        self.g.set_text("Try again!")
+        self.g.set_color(204, 0, 0)
+        self.g.x = 500
+        self.g.y = 310
+
+        self.g.set_on_mouse_down_listener(self.click)
+
+    def click(self, sender, event):
+        print(sender)
+        if event.button == 1:
+            self.screen.game.set_screen(HawkProductions.menu.MenuScreen.MenuScreen())
