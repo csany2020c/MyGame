0d11ebe0d6227733a01176aa672dd4996a6c79b3
Zoro <lorand996@gmail.com>
2021-11-21 22:19:11 +0100

    sok

--- yetifc/yetigame.py
+++ yetifc/yetigame.py
@@ -1,6 +1,9 @@
 from yetifc.yetiscreen import MenuScreen
-from yetifc import *
+from yetifc.yetigamescreen import GameScreen
+from yetifc.yetigamescreen import GameStage
+
 import game
+import pygame
 
 
 class Yeti(game.scene2d.MyGame):
@@ -8,11 +11,16 @@ class Yeti(game.scene2d.MyGame):
     def __init__(self, width: int = 1280, height: int = 720, autorun: bool = False, autosize: bool = False):
         super().__init__(width, height, autorun, autosize)
         self.screen = MenuScreen()
+
         self.set_on_key_down_listener(self.key_down)
 
     def key_down(self, sender, event):
-        print(sender)
-        print(event)
+        if event.key == pygame.K_ESCAPE:
+            quit()
+
+
+
+
 
 
 Yeti().run()
