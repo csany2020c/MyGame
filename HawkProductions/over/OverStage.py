6b48f95e2230ebd41ee267a4088b924b4b6f8182
Fatér László <faterlaszlo@gmail.com>
2021-11-21 13:31:32 +0100

    elírt kód javítása

--- HawkProductions/over/OverStage.py
+++ HawkProductions/over/OverStage.py
@@ -1,5 +1,6 @@
 from HawkProductions.Font import *
 import pygame
+import HawkProductions.menu.MenuScreen
 
 class OverStage(game.scene2d.MyStage):
     def __init__(self):
@@ -10,4 +11,20 @@ class OverStage(game.scene2d.MyStage):
         self.F = Arial()
         self.add_actor(self.F)
         self.F.set_text("Game Over")
+        self.F.x = 500
+        self.F.y = 225
         self.F.set_color(204, 0, 0)
+
+        self.g = Arial()
+        self.add_actor(self.g)
+        self.g.set_text("Try again!")
+        self.g.set_color(204, 0, 0)
+        self.g.x = 500
+        self.g.y = 310
+
+        self.g.set_on_mouse_down_listener(self.click)
+
+    def click(self, sender, event):
+        print(sender)
+        if event.button == 1:
+            self.screen.game.set_screen(HawkProductions.menu.MenuScreen.MenuScreen())
