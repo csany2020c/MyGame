4ea847bce5314a8ca1842324485c9d82fa59e442
BalintKollar852 <balintkollar852@gmail.com>
2021-11-23 13:31:14 +0100

    file beolvasas

--- File/1_feladat/HorvathMark.py
+++ File/1_feladat/HorvathMark.py
@@ -5,14 +5,14 @@ class data:
     def __init__(self, parseString: str) -> None:
         super().__init__()
         fields: List['str'] = parseString.split(";")
-        self.text: int = int(fields[0])
+        self.ev: int = int(fields[0])
         self.nev: str = fields[1]
         self.elet: str = fields[2]
         self.orszag: str = fields[3]
 
 
     def __str__(self) -> str:
-        return "Year = {x}; Name = {y}; Born-death = {txt}; Country = {col}".format(x=self.text, y=self.nev, txt=self.elet, col=self.orszag)
+        return "Year = {x}; Name = {y}; Born-death = {txt}; Country = {col}".format(x=self.ev, y=self.nev, txt=self.elet, col=self.orszag)
 
 
 class olvasas:
@@ -29,5 +29,16 @@ class olvasas:
         for d in datalist:
             print(d)
         f.close()
+
+        print("3. feladat")
+        print("Díjazottak száma: {db} fő ".format(db=len(datalist)))
+
+        max: int = 0
+        for i in range(1, len(datalist)):
+            if datalist[i].ev > datalist[max].ev:
+                max = i
+        print(datalist[max].ev)
+
+        kod: str = input()
 
 olvasas()
