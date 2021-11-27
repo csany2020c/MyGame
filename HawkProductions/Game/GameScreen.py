f80249e4cb61344713d04107ee35186fc44e90ed
Fatér László <faterlaszlo@gmail.com>
2021-11-27 19:42:04 +0100

    javítás (game és a fájlok)

--- HawkProductions/Game/GameScreen.py
+++ HawkProductions/Game/GameScreen.py
@@ -10,4 +10,22 @@ class GameScreen(game.scene2d.MyScreen):
 class GameScreen1(game.scene2d.MyScreen):
     def __init__(self):
         super().__init__()
-        self.add_stage(GameStage())
+        self.add_stage(GameStage1())
+
+
+class GameScreen2(game.scene2d.MyScreen):
+    def __init__(self):
+        super().__init__()
+        self.add_stage(GameStage2())
+
+
+class GameScreen3(game.scene2d.MyScreen):
+    def __init__(self):
+        super().__init__()
+        self.add_stage(GameStage3())
+
+
+class GameScreen4(game.scene2d.MyScreen):
+    def __init__(self):
+        super().__init__()
+        self.add_stage(GameStage4())
