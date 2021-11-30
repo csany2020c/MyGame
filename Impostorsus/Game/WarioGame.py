bdfc6944eb7bbeed4cf8a27e3d86a8cbf84cf409
Bálint <balintkollar852@gmail.com>
2021-11-30 11:59:24 +0100

    Hitboxok

--- Impostorsus/Game/WarioGame.py
+++ Impostorsus/Game/WarioGame.py
@@ -9,7 +9,6 @@ class Wario(game.scene2d.MyGame):
     def __init__(self, width: int = 1280, height: int = 720, autorun: bool = False, autosize: bool = False):
         super().__init__(width, height, autorun, autosize)
         self.screen = MenuScreen()
-a
         if event.key == pygame.K_BACKSPACE:
             self.screen = MenuScreen()
         if event.key == pygame.K_r:
