e07c96ef3f0c36724e3b360520f2fb2878d94174
kistapir1 <kistapir0@gmail.com>
2021-12-01 09:30:43 +0100

    HP képek kivétele, actorok szerkesztése, nevek

--- HawkProductions/over/OverStage.py
+++ HawkProductions/over/OverStage.py
@@ -36,4 +36,32 @@ class OverStage(game.scene2d.MyStage):
     def click(self, sender, event):
         print(sender)
         if event.button == 1:
-            self.screen.game.set_screen(HawkProductions.Select.SelectScreen.SelectScreen2())
+            self.screen.game.set_screen(HawkProductions.Select.SelectScreen.SelectScreen())
+
+
+class OverStage2(game.scene2d.MyStage):
+    def __init__(self):
+        super().__init__()
+        pygame.mixer.init()
+        pygame.mixer.music.load("../HawkProductions/Music/Over.wav")
+        pygame.mixer.music.play(-1)
+        self.F = Arial()
+        self.add_actor(self.F)
+        self.F.set_text("Túl magasra repültél!")
+        self.F.x = 500
+        self.F.y = 225
+        self.F.set_color(204, 0, 0)
+
+        self.g = Arial()
+        self.add_actor(self.g)
+        self.g.set_text("Try again!")
+        self.g.set_color(204, 0, 0)
+        self.g.x = 500
+        self.g.y = 310
+
+        self.g.set_on_mouse_down_listener(self.click)
+
+    def click(self, sender, event):
+        print(sender)
+        if event.button == 1:
+            self.screen.game.set_screen(HawkProductions.menu.MenuScreen.MenuScreen())
