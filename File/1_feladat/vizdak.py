a80cea210468fbdf13887b90d7dbc6bab632f428
Da-WEED <zsebokdavidf@gmail.com>
2021-12-07 13:03:36 +0100

    orai

--- File/1_feladat/vizdak.py
+++ File/1_feladat/vizdak.py
@@ -6,39 +6,51 @@ class Data:
 
     def __init__(self, parseString: str) -> None:
         super().__init__()
-
         print(parseString)
-        fields: List['str'] = parseString.split(" ")
+        fields: List['str'] = parseString.split(";")
+        self.ev: int = int(fields[0])
+        self.nev: str = fields[1]
+        self.elet: str = fields[2]
+        self.kod: str = fields[3]
 
-
-        self.text: str = ""
-        for i in range(5, len(fields)):
-            self.text += str(fields[i])
-            if i < len(fields) - 1:
-                self.text += " "
+    def __str__(self) -> str:
+        return "{x};   {y};   {txt};   {col}".format(x=self.ev, y=self.nev, txt=self.elet, col = self.kod)
 
 
 class Main:
 
     def __init__(self) -> None:
         super().__init__()
-        f: TextIO = open("!_Spec/orvosi_nobeldijak.txt", "r")
+        f: TextIO = open("!_Spec//orvosi_nobeldijak.txt", "r")
         content: str = f.read()
-        #print("Content:")
-        print(content)
         lines: List['str'] = content.split(sep="\n")
         print("Split content")
         print(lines)
         print("Load to List")
         datalist: List['Data'] = list()
-
         for i in range(1, len(lines) - 1):
             d = Data(lines[i])
             datalist.append(d)
-        print("Print list")
-        for d in datalist:
-            print(d)
+
+
+
+        print("...")
         f.close()
+
+        print("3. feladat")
+        print("Díjazottak száma: {db} fő ".format(db=len(datalist)))
+
+        max: int = 0
+        for i in range(1, len(datalist)):
+            if datalist[i].ev > datalist[max].ev:
+                max = i
+        print(datalist[max].ev)
+
+        print("5. feladat")
+
+        kod: str = input()
+
+        db: int = 0
 
 
 Main()
