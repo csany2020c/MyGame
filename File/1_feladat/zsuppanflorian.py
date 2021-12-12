3f765a22151e42fa40de35b26b574175a79a27a5
Zsuppán Flórián <zs.florian25@gmail.com>
2021-12-12 18:21:00 +0100

    zsupp voltam

--- File/1_feladat/zsuppanflorian.py
+++ File/1_feladat/zsuppanflorian.py
@@ -17,6 +17,7 @@ class Data:
             self.Halalozas = int(szh[1])
         self.Orszagkod: str = fields[3]
 
+
     def __str__(self) -> str:
         return "Ev = {x}; Nev = {y}; SzuletesHalalozas = {txt}; Sz = {sz} H = {h}  Orszagkod = {col}".format(x=self.Ev, y=self.Nev, txt=self.SzuletesHalalozas, col = self.Orszagkod, sz= self.Szuletes, h= self.Halalozas)
 
@@ -28,41 +29,43 @@ class Main:
         lines: List['str'] = content.split(sep="\n")
         dijazottak: List['Data'] = list()
         for i in range(1, len(lines) - 1):
-            d = Data(lines[i])
-            dijazottak.append(d)
+            dijazottak.append(Data(lines[i]))
         f.close()
 
 
         print("3. feladat")
         print("Díjazottak száma: {db} fő ".format(db=len(dijazottak)))
 
-
         max: int = 0
-
         for i in range(1, len(dijazottak)):
-
             if dijazottak[i].Ev > dijazottak[max].Ev:
                 max = i
-
         print(dijazottak[max].Ev)
 
-
-        kod: str = input()
+        kod: str = input().upper().strip()
 
         db: int = 0
-
+        utolso_megtalalt: Data
         for k in range(0, len(dijazottak)):
-
             if dijazottak[k].Orszagkod == kod:
                 db += 1
                 utolso_megtalalt = dijazottak[k]
-
         if db == 0:
             print("A megadott országból nem volt díjazott!")
         elif db == 1:
             print(utolso_megtalalt)
         else:
             print("A megadott országból {db} fő díjazott volt!".format(db = db))
+
+
+         # iterátoros végigjárás
+        for it in dijazottak:
+            print(it)
+
+         # index alapú végigjárás
+        for index in range(0, len(dijazottak)):
+            print(str(index) + " ---- " + str(dijazottak[index]))
+
 
         orszagok: dict = dict()
         for k in range(0, len(dijazottak)):
@@ -75,20 +78,11 @@ class Main:
             if v > 5:
                 print("{k} {v}".format(k=k, v=v))
 
-        db80: int = 0
-        for it in dijazottak:
-            if it.Ev >= 1980 and it.Ev <= 1989:
-                print(it.Nev)
-                db80 += 1
-        print("Az 1980-as években {db80} díjazott volt.".format(db80 = db80))
-
-
-        for it in dijazottak:
-            if it.Ev <= 1969 and it.Orszagkod == str("USA") or it.Ev >= 1960 and it.Orszagkod == str("S"):
-                print(it.Nev)
-
 
 Main()
+
+
+
 
 
 
