525c21cda8d0fa9aa5521f0f2af1cdbcb0fd3d25
tanulo <doramarton20@gmail.com>
2021-12-14 12:02:35 +0100

    vegremukodikapycharm

--- File/3_feladat/szabobalintk.py
+++ File/3_feladat/szabobalintk.py
@@ -22,7 +22,7 @@ class Nemertemafeladatot:
         content: str = f.read()
         lines: List['str'] = content.split(sep="\n")
         datalist: List['Data'] = list()
-        for i in range(0, len(lines) - 1):
+        for i in range(1, len(lines) - 0):
             d = Data(lines[i])
             datalist.append(d)
 
