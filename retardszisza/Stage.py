45bf148cbea314f776ecab91affb17b555f379be
tanulo <rdonat123@gmail.com>
2022-01-07 10:35:02 +0100

    asdasdasdasdasdasdasd

--- retardszisza/Stage.py
+++ retardszisza/Stage.py
@@ -18,16 +18,23 @@ class GameStage(game.scene2d.MyStage):
         self.a = UtActor()
         self.b = szisza()
         self.c = kocsi1()
+        self.fal = FalActor()
+        self.palyaszele = Palyaszele()
 
         self.add_actor(self.a)
         self.add_actor(self.b)
         self.add_actor(self.c)
+        self.add_actor(self.fal)
+        self.add_actor(self.palyaszele)
         #pos
         self.b.x = 100
         self.b.y = 550
 
         self.c.x = 600
         self.c.y = 550
+
+        self.fal.x = -250
+        self.palyaszele.y = 720
         #hitbox
         self.b.hitbox_scale_h = 1
         self.b.hitbox_scale_w = 1
@@ -47,11 +54,24 @@ class GameStage(game.scene2d.MyStage):
     def act(self, delta_time: float):
         super().act(delta_time)
         Overlapsasd: bool = False
+        kocsirespawn: bool = False
+        Palyaszel: bool = False
 
         for ASDASD in self.actors:
                 if self.c.overlaps(self.b):
                     Overlapsasd = True
                     break
+                if self.c.overlaps(self.fal):
+                    kocsirespawn = True
+                if self.b.overlaps(self.palyaszele):
+                    Palyaszel = True
+
 
         if Overlapsasd:
             self.screen.game.set_screen(retardszisza.menu_halal.HalalScreen.halalscreen()) #HALAL
+
+        if kocsirespawn:
+            self.c.x = 600
+
+        if Palyaszel:
+            self.b.y = 550
