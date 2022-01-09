31bd6fcf84189dc14d52165bfc408f889056475e
Zoro <lorand996@gmail.com>
2022-01-09 20:41:19 +0100

    lehet lőni

--- yetifc/yetigamescreen.py
+++ yetifc/yetigamescreen.py
@@ -21,6 +21,10 @@ class BulletCount(game.scene2d.MyLabel):
     def __init__(self):
         super().__init__("Bullets:")
 
+class Bullet(game.scene2d.MyActor):
+    def __init__(self):
+        super().__init__("Images/bullet.png")
+
 class GameScreen(game.scene2d.MyScreen):
     def __init__(self):
         super().__init__()
@@ -30,12 +34,14 @@ class GameStage(game.scene2d.MyStage):
     def __init__(self):
         self.ronstage : bool = True
         self.sonstage : bool = False
+        self.kpushed : bool = False
         super().__init__()
         self.character = Character()
         self.gamebg = GameBG()
         self.revolver = Revolver()
         self.shotgun = Shotgun()
         self.bulletcount = BulletCount()
+        self.bullet = Bullet()
         self.add_actor(self.gamebg)
         self.add_actor(self.character)
         self.add_actor(self.bulletcount)
@@ -44,11 +50,15 @@ class GameStage(game.scene2d.MyStage):
         self.character.z_index = 1
         self.revolver.z_index = 2
         self.shotgun.z_index = 2
+        self.bullet.z_index = 2
         self.revolver.y = 410
         self.revolver.x = 70
         self.shotgun.y = 400
         self.shotgun.x = 30
+        self.bullet.y = 450
+        self.bullet.x = 140
         self.add_actor(self.revolver)
+        self.bullet.width = 100
         self.character.y = 400
         self.set_on_key_press_listener(self.key_down)
         self.set_on_key_down_listener(self.weapon_switch)
@@ -67,6 +77,7 @@ class GameStage(game.scene2d.MyStage):
             self.shotgun.x = self.shotgun.x - 10
 
     def weapon_switch(self, sender, event):
+
         if event.key == pygame.K_2:
             if self.ronstage == True:
                 self.remove_actor(self.revolver)
@@ -93,9 +104,27 @@ class GameStage(game.scene2d.MyStage):
             self.character.x = self.character.x - 30
             self.shotgun.x = self.shotgun.x - 30
             self.revolver.x = self.revolver.x - 30
+            if self.ronstage == True:
+                self.kpushed = True
+                self.bullet.x = self.revolver.x + 70
+                self.bullet.y = self.revolver.y + 40
             if self.sonstage == True:
                 pygame.mixer.music.load("Images/shotgun.wav")
                 pygame.mixer.music.play()
+
+
+    def act(self, delta_time: float):
+        super().act(delta_time)
+        if self.kpushed == True:
+            if self.ronstage == True:
+                self.add_actor(self.bullet)
+
+                self.bullet.x = self.bullet.x + 10
+
+
+
+
+
 
 
 
