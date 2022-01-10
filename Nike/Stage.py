631d8fdbb9cd824cd72997b91db0cb6fb478fa0d
Rigó <rdonat123@gmail.com>
2022-01-10 19:20:29 +0100

    projekt

---
+++ Nike/Stage.py
@@ -0,0 +1,48 @@
+import game
+from Nike.Screen import *
+from Nike.Actor import *
+import Nike.Screen
+import random
+import pygame
+from game.simpleworld.ShapeType import ShapeType
+
+class MenuStage(game.scene2d.MyStage):
+    def __init__(self):
+        super().__init__()
+        self.FatJordan = FatJordan()
+        self.add_actor(FatJordan())
+        self.text = MenuText()
+        self.add_actor(self.text)
+        self.text.set_text("Fat Jordan")
+        self.text.set_alpha(500)
+        self.text.set_width(80)
+        self.text.set_height(80)
+        self.text.x += 500
+        self.text.y += 150
+        self.start = MenuText()
+        self.add_actor(self.start)
+        self.start.set_text("Start")
+        self.start.set_alpha(500)
+        self.start.set_width(80)
+        self.start.set_height(80)
+        self.start.x += 300
+        self.start.y += 250
+        self.start.set_on_mouse_down_listener(self.play)
+        self.exit = MenuText()
+        self.add_actor(self.exit)
+        self.exit.set_text("Exit")
+        self.exit.set_alpha(500)
+        self.exit.set_width(80)
+        self.exit.set_height(80)
+        self.exit.x += 850
+        self.exit.y += 250
+        self.exit.set_on_mouse_down_listener(self.exitbut)
+
+
+    def play(self, sender, event):
+        print(sender)
+        print(event)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                self.screen.game.set_screen(Nike.NikeScreen.Game()
+                quit()
