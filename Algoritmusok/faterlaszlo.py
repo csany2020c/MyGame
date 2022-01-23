524dd85ed4667ef590ac952840f3f25a8ccf2af3
Fatér László <faterlaszlo@gmail.com>
2022-01-23 21:27:47 +0100

    feladat

--- Algoritmusok/faterlaszlo.py
+++ Algoritmusok/faterlaszlo.py
@@ -1,4 +1,5 @@
 from typing import List
+from time import time
 
 # def faktoralis(asd: int) -> int:
 #     szorzat = 1
@@ -7,29 +8,38 @@ from typing import List
 #     return szorzat
 
 
-#print(faktoralis(6))
+# print(faktoralis(6))
 
-#2. feladat
-#print(8 % 3)
+# 2. feladat
+# print(8 % 3)
 
-def fuggveny(n: int) -> List['str']:
-     lista: List['int'] = list()
-     #bemenet: int = int(input())
-     for i in range(1, n + 1):
-         if n % i == 0:
-             lista.append(i)
-     return lista
+# primszam fuggveny
+def fuggveny(n: int) -> list[int]:
+    lista: List['int'] = list()
+    # bemenet: int = int(input())
+    for i in range(1, n + 1):
+        if n % i == 0:
+            lista.append(i)
+    return lista
 
-     for i in fuggveny(26):
-         print(i)
 
-#print(fuggveny(26))
+# for i in fuggveny(26):
+#     print(i)
 
-#3.feladat
-def fuggveny1(n: int) -> bool:
-    return len(fuggveny(n)) == 2
+# print(fuggveny(26))
 
+# 3.feladat
+# ts1 = time()
+# def fuggveny1(n: int) -> bool:
+#     return len(fuggveny(n)) == 2
+#
 # for i in range(0, 126):
-#     print("{szam} {primszam}".format(szam=i, primszam=fuggveny1(i)))
+#      print("{szam} {primszam}".format(szam=i, primszam=fuggveny1(i)))
 
-
+ts1 = time()
+for i in range(100000, 1000000):
+    primszame = fuggveny(i)  # Prímszám eldöntő függvény helye
+    if primszame:
+        print(i)
+ts2 = time()
+print("Az algoritmus {mp} másodpercig futott.".format(mp=(ts2 - ts1)))
