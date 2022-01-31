b6548cd7d1dc234c95a9e216c89a9ad2a9d432db
kistapir1 <kistapir0@gmail.com>
2022-01-31 08:57:20 +0100

    Info xdlol

--- Kancsalmate27megilyenek/DevActor.py
+++ Kancsalmate27megilyenek/DevActor.py
@@ -5,7 +5,4 @@ import pygame
 class DevActor(game.scene2d.MyActor):
     def __init__(self, image_url: str = "devs.png"):
         super().__init__(image_url)
-        self.info = pygame.display.Info()
-        self.width = self.info.current_w
-        self.height = self.info.current_h
         self.set_size(400, 200)
--- Kancsalmate27megilyenek/InfoStage.py
+++ Kancsalmate27megilyenek/InfoStage.py
@@ -3,9 +3,20 @@ import game
 import pygame
 from Kancsalmate27megilyenek.InfoScreen import *
 from Kancsalmate27megilyenek.DevActor import *
+import Kancsalmate27megilyenek.MenuScreen
 class InfoStage(game.scene2d.MyStage):
     def __init__(self):
         super().__init__()
+        self.info = pygame.display.Info()
+        self.width = self.info.current_w
+        self.height = self.info.current_h
         pygame.init()
         Dev = DevActor()
         self.add_actor(Dev)
+        Dev.set_position(self.width/2 -Dev.get_width()/2,100)
+        self.set_on_key_down_listener(self.back)
+
+
+    def back(self,sender,event):
+        if event.key == pygame.K_ESCAPE:
+            self.screen.game.set_screen(Kancsalmate27megilyenek.MenuScreen.MenuScreen3())
