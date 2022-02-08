8c7361560f57e8004afd5b043a6828fe5ebfaad2
BYAX12 <csongor.nemeth05@gmail.com>
2022-02-08 08:20:38 +0100

    NCS binaris2

--- Algoritmusok/nemethcsongor.py
+++ Algoritmusok/nemethcsongor.py
@@ -1,9 +1,7 @@
 def binaris(bemenet):
     b = ''
-    while True:
-        if bemenet == 0:
-            break
-        elif (bemenet % 2) == 0:
+    while bemenet > 0:
+        if bemenet % 2 == 0:
             bemenet = bemenet // 2
             b = '0' + b
         else:
@@ -12,4 +10,4 @@ def binaris(bemenet):
     return b
 
 
-print("A szám bináris értéke " + str(binaris(124)))
+print("A szám bináris értéke " + str(binaris(10)))
