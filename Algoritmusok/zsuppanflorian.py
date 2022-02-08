2b50a8b4bd06c4e8c21734cc4ed642dec55e0ba3
zsuppanvagyok <zs.florian25@gmail.com>
2022-02-08 08:37:46 +0100

    lul

--- Algoritmusok/zsuppanflorian.py
+++ Algoritmusok/zsuppanflorian.py
@@ -14,13 +14,13 @@
 # from sympy import *
 # print(isprime(2))
 
-n = 234
-for i in range(2, n+1):
-    if n % i == 0:
-        ertek = 1
-        for k in range(2, (i//2)+1):
-            if(i % k == 0):
-                ertek = 0
-                break
-        if(ertek == 1):
-            print(i)
+# n = 234
+# for i in range(2, n+1):
+#     if n % i == 0:
+#         ertek = 1
+#         for k in range(2, (i//2)+1):
+#             if(i % k == 0):
+#                 ertek = 0
+#                 break
+#         if(ertek == 1):
+#             print(i)
