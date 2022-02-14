bf8c7ce736e76b4868c66bab7355681a8edc933f
tuskeb <tuskebganz@gmail.com>
2022-02-14 10:43:52 +0100

    HF

---
+++ Algoritmusok/!_HF.py
@@ -0,0 +1,28 @@
+# 0.0 Készítsen függvényt, amelynek bemenete egy lista és egy szám. Válogassa ki a kimenetre egy új listába azokat a számokat, amelyek a bemeneti listában a bemeneti számmal maradék nélkül oszthatók.
+
+# 0.1 Készítsen függvényt amelynek bemenete egy lista, kimenete pedig logikai típus. A visszaadott érték legyen igaz, ha a listában van 0. Ha nincs, akkor hamis.
+
+# 1. Készítsen függvényt, amelynek a bemenete 2 szám, a kimenete pedig a kisebb szám a kettő közül.
+# A neve min legyen.
+
+# 2. Készítsen függvényt, amelynek a bemenete egy lista, a kimenete pedig a legkisebb szám a lista elemei közül.
+# A neve minlist legyen.
+# http://szit.hu/doku.php?id=oktatas:programozas:programozasi_tetelek:mondatszeru_leiras
+
+# 3. Készítsen függvényt, amelynek 3 szám a bemenete.
+#   a mértani sorozat első tagja (a1)
+#   egy mértani sorozat kvóciense (q)
+#   a legenerált sorozat hossza. (n)
+# Generálja le az első n értéket egy listába. (mértani sorozatát)
+# https://hu.wikipedia.org/wiki/M%C3%A9rtani_sorozat
+#
+
+# 4. Készítsen függvényt, amelynek bemenete egy lista, visszaadott értéke pedig a lista elemeinek az összege.
+
+# 5. Számítsa ki a 3-as és 4-es függvény felhasználásával egy mértani sorozat első n elemének az összegét.
+# A függvény kimenete egy szám legyen, a bemenete pedig a mértani sorozat generálásához szükséges értékek.
+
+# 6. Készítsen függvényt, amelynek bemenete 3 szám, amely egy másodfokú a,b,c paraméterei (float). ax^2 + bx + c
+# https://hu.wikipedia.org/wiki/M%C3%A1sodfok%C3%BA_egyenlet
+# A visszaadott érték egy lista legyen, amely az egyenlet megoldásait tartalmazza.
+# Amennyiben az egyenletnek nincs megoldása a valós számok halmazán, üres listával térjen vissza.
