1fb7e67fae9d00e7809f5e7a7b0c58e9e5bebd97
tuskeb <tuskebganz@gmail.com>
2022-02-22 10:03:53 +0100

    HF3

---
+++ Algoritmusok/!_HF3.py
@@ -0,0 +1,26 @@
+#https://hu.wikipedia.org/wiki/Mersenne-pr%C3%ADmek
+# A matematikában Mersenne-prímeknek nevezzük a kettő-hatványnál eggyel kisebb, azaz a 2n ‒ 1 alakban felírható prímszámokat, ahol n szintén prímszám.
+#
+# 1.a Készítsen függvényt, amelynek a bemenete a hatványkitevő. Visszaadott értéke igaz vagy hamis az alapján, hogy Mesenne pím-e.
+# A 2^n-1 alakban felírt számok nagy valószínűséggel prímek, főleg ha n is prím. De nem biztos, tehát meg kell vizsgálni.
+
+# 1.b Keressen Mersenne prímeket. Az előző függvényt felhasználva keresse őket úgy, hogy a fenti függvény hatványkitevő bemenete prímszám legyen,
+# A 2^n-1 alakban felírt számokra, ahol n prím minden esetben írja ki, hogy prím-e vagy nem.
+
+
+
+# https://hu.wikipedia.org/wiki/Fibonacci-sz%C3%A1mok
+# 2. Készítsen függvényt, amely listában adja vissza az első n darab Fibonacci számot. Az n a függvény bemenete.
+
+
+# https://hu.wikipedia.org/wiki/Legkisebb_k%C3%B6z%C3%B6s_t%C3%B6bbsz%C3%B6r%C3%B6s
+# 3. Készítsen függvényt, amely két szám legkisebb közös többszörösét adja eredményül.
+
+
+# https://hu.wikipedia.org/wiki/Legnagyobb_k%C3%B6z%C3%B6s_oszt%C3%B3
+# 4. Készítsen függvényt, amely két szám legnagyobb közös osztóját adja eredményül.
+
+
+# https://hu.wikipedia.org/wiki/Szfenikus_sz%C3%A1mok
+# Szfenikus számok
+# 5. Fügvénnyel döntse el egy számról szfenikus szám-e.
