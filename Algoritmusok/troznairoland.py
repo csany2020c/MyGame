cb6b63047222ed737248ef390557ecbe0bb9f0ed
Da-WEED <zsebokdavidf@gmail.com>
2022-02-22 08:34:36 +0100

    Revert "Helyiérték"
    This reverts commit 0c64943667b6e9cef990af116cecff8d15f2f39e.

--- Algoritmusok/troznairoland.py
+++ Algoritmusok/troznairoland.py
@@ -200,22 +200,13 @@ from typing import List
 #print(relativprim(6,35))
 
 
-# Hazi 2.3:
-def helyiertek(be:int) -> List['int']:
-    ki: List['int'] = list()
-    while be % 10 != 0:
-        ki.append(be % 10)
-        be = be // 10
-    if len(ki) == 0:
-        ki.append(0)
-    ki.reverse()
-    return ki
-
-def helyiertek2(be:int) -> List['int']:
-    ki: List['int'] = list()
-    for c in str(be):
-        ki.append(int(c))
-    return ki
+#Hazi 2.3:
+#def helyiertek(be:int) -> List['int']:
+    #ki: List['int'] = list()
+    #marad = 0
+    #if be % 10 > 0:
+        #ki.append()
+    #else:
 
 
-print(helyiertek2(be = 623))
+#helyiertek(be = 623)
