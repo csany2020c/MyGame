3fd4249e50fd172152c484f05aa1d4d04b2ce7dc
kistapir1 <kistapir0@gmail.com>
2022-02-22 11:28:36 +0100

    kmjkn

--- Algoritmusok/czirakileila.py
+++ Algoritmusok/czirakileila.py
@@ -25,5 +25,24 @@ class beolvasas:
 
 #print(osztoparok(int(input())))
 
+#def feladat00(lista: List) -> int:
+ #   for i in range:
+
+
+
+
+
+
+# 1. Készítsen függvényt, amely eldönti egy számról, hogy "tökéletes"-e.
+def tokeletesszam(szam: int) -> int:
+    szamfele: int = szam // 2 + 1
+    osszeg: int = 0
+    for x in range(1, szamfele):
+        if szam % x == 0:
+            osszeg = osszeg + x
+        if szam == osszeg:
+            return
+
+
 
 
