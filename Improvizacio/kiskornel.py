356ec66bc6b67e1895ac6d6744914ea78bd30747
4kisk <8kiskornel@gmail.com>
2022-05-18 22:03:55 +0200

    asd

--- Improvizacio/kiskornel.py
+++ Improvizacio/kiskornel.py
@@ -18,4 +18,23 @@ encsukam.szin = "Feher"
 print(encsukam.femstoplis)
 
 
+class kutya:
+    def __init__(self):
+        super().__init__()
+        self.eletkor:int = 2
+        self.ivartalanitott:bool = True
+        self.fajta:str = "Pitbull"
+        self.fajtiszta:bool = True
+
+
+
+class ruha:
+    def __init__(self):
+        super().__init__()
+        self.arforintban:int = 6000
+        self.garanciavan:bool = True
+        self.marka:str = "Nike"
+        self.szin:str = "Fekete"
+
+
 
