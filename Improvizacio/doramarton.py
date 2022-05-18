33fcd5e69a1686bd2daa5b769bd3da2bcad37b79
Win10 <doramarton20@gmail.com>
2022-05-18 20:36:23 +0200

    asd

--- Improvizacio/doramarton.py
+++ Improvizacio/doramarton.py
@@ -17,3 +17,36 @@ solyom.ropkepes = True
 solyom.ragadozo = True
 
 print(solyom.enekes)
+
+
+class idojaras:
+    def __init__(self):
+        super().__init__()
+        self.esos: bool
+        self.napos: bool
+        self.felhos: bool
+        self.tornado: bool
+
+
+ido = idojaras
+ido.esos = True
+ido.napos = False
+ido.felhos = False
+ido.tornado = True
+print(ido.esos, ido.napos, ido.felhos, ido.tornado)
+
+
+class fa:
+    def __init__(self):
+        super().__init__()
+        self.fajta: str
+        self.szelesseg: int
+        self.magassag: int
+        self.rohade: bool
+
+fasize = fa
+fasize.fajta = "bukk"
+fasize.szelesseg = 50
+fasize.magassag = 500
+fasize.rohade = True
+print(fasize.fajta, fasize.szelesseg, fasize.magassag, fasize.rohade)
