5c1a97149861895857bf7ad3e3cda8ad27afe665
HBoldi777 <71324114+HBoldi777@users.noreply.github.com>
2022-05-18 08:39:55 +0200

    asd

---
+++ Improvizacio/mandliivett.py
@@ -0,0 +1,16 @@
+class Golya:
+    def __init__(self):
+        self.tudrepulni = True
+        self.csorhossz = 15
+        self.suly = 120
+        self.magassag = 100
+
+Z = Golya()
+
+Z.tudrepulni = True
+Z.csorhossz = 16
+Z.suly = 125
+Z.magassag = 105
+
+print(Z.suly)
+
