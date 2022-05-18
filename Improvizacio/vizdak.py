de722da4c9e4fc00fd60f0c84276bd4049e72f94
vizdakmate <mate.vizdak@gmail.com>
2022-05-18 19:58:02 +0200

    hazi proba

--- Improvizacio/vizdak.py
+++ Improvizacio/vizdak.py
@@ -1,23 +1,53 @@
 from typing import List
+from typing import TextIO
+
 
 class Alma:
-        nagysaga: int
+    def __init__(self):
+        super().__init__()
 
-        def __init__(self, parse: str) -> None:
-            super().__init__()
-            fields: List['str'] = parse.split(" ")
-            self.szin: str = "piros"
-            self.nagysaga: int = int("5")
-            self.erett: bool
-            self.finom: bool
+        self.szin: str = "piros"
+        self.nagysaga: int = 5
+        self.erett: bool
+        self.finom: bool
 
 
 myalma = Alma()
 
 myalma.szin = "piros"
-myalma.nagysaga = "5"
+myalma.nagysaga = 5
 myalma.erett = True or False
 myalma.finom = True or False
 #hf ket osztalyt leterhozni es peldanyolakat
 
-print(myalma.szin)
+print(myalma.erett)
+
+
+class Haz:
+    def __init__(self):
+        super().__init__()
+        self.szin: str = "kék"
+        self.nagysaga: int = 150
+        self.csaladi: bool
+
+myHaz = Haz()
+
+myHaz.szin = "kék"
+myHaz.nagysaga = 150
+myHaz.csaladi = True or False
+
+print(myHaz.csaladi)
+
+class TV:
+    def __init__(self):
+        super().__init__()
+        self.minoseg: str = "HD"
+        self.nagysaga: int = 125
+        self.szin: str = "fekete"
+
+
+myTV = TV()
+myTV.szin = "fekete"
+myTV.nagysaga = 125
+myTV.minoseg = "HD"
+print()
