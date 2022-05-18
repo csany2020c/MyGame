b162193b81179a6550f809275cd0e26c7387ca8d
marcontamas <marcontomika@gmail.com>
2022-05-18 08:41:07 +0200

    *

---
+++ Improvizacio/tothakos.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Motor:
+    def __init__(self):
+        self.gyarto: str = str()
+        self.tipus: str = str()
+        self.kobcenti: int = int()
+        self.szin: str = str()
+        self.loero: int = int()
+        self.valto: bool = bool()
+
+motor = Motor()
+motor.gyarto = "KTM"
+motor.tipus = "Duke"
+motor.kobcenti = 125
+motor.szin = "Fekete"
+motor.loero = 15
+motor.valto = True
+
+#hf: +2osztály létrehozása, kirása
