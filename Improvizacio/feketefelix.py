d9603b3f888f1228b79f2eb07a782b6cb5990887
Laszlo9 <71323987+Laszlo9@users.noreply.github.com>
2022-05-18 08:39:53 +0200

    asd

---
+++ Improvizacio/feketefelix.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class huto:
+    def __init__(self):
+        self.szin = "Fekete"
+        self.tipus: str = "Mely"
+        self.szeleseg: int + 0
+        self.magassag: int + 0
+
+adat = huto()
+adat.szin = "Piros"
+print(adat.szin)
+
+
+
+
+
+
+
+
+
+
+
