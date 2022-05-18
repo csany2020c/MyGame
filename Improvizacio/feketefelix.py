cf0acb42f0ee77205bc30d4eed548251e03b109b
feketefelix555 <fekete.felix2005@gmail.com>
2022-05-18 16:39:22 +0200

    asd

--- Improvizacio/feketefelix.py
+++ Improvizacio/feketefelix.py
@@ -2,14 +2,27 @@ from typing import List
 
 class huto:
     def __init__(self):
-        self.szin = "Fekete"
-        self.tipus: str = "Mely"
+        self.szin: "szine"
+        self.tipus: "fajtaja"
         self.szeleseg: int + 0
         self.magassag: int + 0
+        self.marka: "markaneve"
+        self.teljesitmeny: "Wolt szam"
 
 adat = huto()
+adat.marka = "Gorenje"
 adat.szin = "Piros"
+adat.szeleseg = 600
+adat.magassag = 1940
+#ezek mm-be megadott értékek
+adat.tipus = "Standard"
+adat.teljesitmeny = "70 W"
+print(adat.marka)
 print(adat.szin)
+print(adat.szeleseg)
+print(adat.magassag)
+print(adat.teljesitmeny)
+
 
 
 
