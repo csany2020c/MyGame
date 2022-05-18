8e13688f30e67801c3d4d930d444e8d54ba847a9
rigodonat <rdonat123@gmail.com>
2022-05-18 21:27:09 +0200

    hf

--- Improvizacio/rigodonat.py
+++ Improvizacio/rigodonat.py
@@ -16,6 +16,44 @@ MyMotor.vegsebesseg = 200
 MyMotor.valto = True
 MyMotor.hp = 74
 
-print(MyMotor)
+print(MyMotor.hp)
 
 #hf:még két osztály létrehozása és kirása
+
+
+
+class bicikli:
+    def __init__(self) -> None:
+        super().__init__()
+        self.gyarto: str = str
+        self.sebessegbaltofkozatszam: int = int
+        self.kerekmeret: int = int
+        self.csomagtarto: bool
+
+MyBicikli = bicikli()
+MyBicikli.gyarto = "KTM"
+MyBicikli.sebessegbaltofkozatszam = 24
+MyBicikli.kerekmeret = 28
+MyBicikli.csomagtarto = False
+
+print(MyBicikli.csomagtarto)
+print(MyBicikli.gyarto)
+
+
+
+class bukosisak:
+    def __init__(self):
+        self.gyarto: str = str
+        self.szemuveg: bool
+        self.tipus: str = str
+        self.meret: int = int
+
+MyBukosisak = bukosisak()
+MyBukosisak.gyarto = "Thor"
+MyBukosisak.szemuveg = True
+MyBukosisak.meret = 58
+MyBukosisak.tipus = "cross"
+
+print(MyBukosisak.tipus)
+print(MyBukosisak.meret)
+print(MyBukosisak.szemuveg)
