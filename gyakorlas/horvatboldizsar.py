c2c2c133bed3606b3a03e14febdf76555efa096e
HBoldi777 <71324114+HBoldi777@users.noreply.github.com>
2022-05-23 21:08:04 +0200

    .

--- gyakorlas/horvatboldizsar.py
+++ gyakorlas/horvatboldizsar.py
@@ -1,42 +1,72 @@
 from typing import List
-from typing import TextIO
+
 
 class Feladat:
-    def __init__(self):
-        f: TextIO = open("toto.txt")
-        tartalom: str = f.read()
+    def __init__(self) -> None:
+        olvasas = open("toto.txt", 'r', encoding="utf-8").read().strip().split(sep="\n")
+        lista: List[Data] = list()
+        for i in range(1, len(olvasas)):
+            lista.append(Data(olvasas[i]))
 
-        print(tartalom)
+        # 3.feladat
 
-        sor: List['str'] = tartalom.split(sep="\n")
-        print(sor)
-
-#3.feladat
         print("3.feladat")
-        sorokszama = 0
-        for i in range(len(sor)):
-            sorokszama += 1
-        print(sorokszama)
+        fordulok = 0
+        for z in range(len(lista)):
+            fordulok += lista[z].fordulo
+        print("{} forduló volt összesen".format(fordulok))
 
-#4.feladat
-        # print("4.feladat")
-        # telitalalat = 0
-        # for i in range(sorokszama):
-        #     telitalalat
-        # print(telitalalat)
-        #
+        # 4.feladat
+
+        print("4.feladat")
+        teli = 0
+        for x in range(len(lista)):
+            teli += lista[x].t13p1
+        print("{} db telitalálatos szelvény van.".format(teli))
+
+        # 5.feladat
+
+        print("5.feladat")
+        telikifizetett = 0
+        for y in range(len(lista)):
+            telikifizetett += lista[y].t13p1 * lista[y].ny13p1
+        print("{} Ft a kifizetett pénz összege.".format(telikifizetett))
+
+        # 8.feladat
+
+        print("8.feladat")
+        dontetlenek = 0
+        for o in range(0, len(lista)):
+            for o in str(lista[o].eredmenyek):
+                if o == "X":
+                    dontetlenek += 1
+        print("{} db döntetlen volt.".format(dontetlenek))
+
+        # 9.feladat
+
+        print("9.feladat")
+        vandxd = 0
+        for q in range(0, len(lista)):
+            for q in str(lista[q].eredmenyek):
+                if q == "X":
+                    vandxd += 1
+                    if vandxd == 1:
+                        print("Van döntetlen.")
+
+
 
 class Data:
-    def __init__(self, vesszo: str):
-        szelet: List['str'] = vesszo.split(",")
+    def __init__(self, szokoz: str) -> None:
+        szelet: List['str'] = szokoz.split(" ")
         self.ev: int = int(szelet[0])
         self.het: int = int(szelet[1])
         self.fordulo: int = int(szelet[2])
         self.t13p1: int = int(szelet[3])
         self.ny13p1: int = int(szelet[4])
-        self.eredmenyek: str = szelet[5]
+        self.eredmenyek: str = str(szelet[5])
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "Év = {}; Hét = {}; Forduló = {}; T13p1 = {}; Ny13p1 = {}; Eredmények = {}".format(self.ev, self.het, self.fordulo, self.t13p1, self.ny13p1, self.eredmenyek)
+
 
 Feladat()
