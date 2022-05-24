050cc779636afb433530438ee3dc557fcd8577e8
SzaboBalintK <71377767+SzaboBalintK@users.noreply.github.com>
2022-05-24 13:53:14 +0200

    par cucc

--- Improvizacio/nemethcsababence.py
+++ Improvizacio/nemethcsababence.py
@@ -1,40 +1,5 @@
 from typing import TextIO, List
 
-'''
-class jatek:
-    def __init__(self) -> None:
-        super().__init__()
-        self.nev: str = str("A játék neve: ")
-        self.kidatum: int = int(0)
-        self.kategoria: str = str("Minősége: AAA")
-        self.ar: int = int(0)
-
-
-bement = jatek()
-bement.nev = str(input("A játék neve: "))
-bement.kidatum = int(input("Kiadás éve: "))
-bement.kategoria = str(input("Minősége: "))
-bement.ar = 35
-print(bement.nev, bement.kidatum, bement.kategoria, bement.ar)
-
-bement2 = jatek()
-bement2.nev = "Valorant"
-bement2.kidatum = 2019
-bement2.kategoria = "AA"
-bement2.ar = 0
-print(bement2.nev, bement2.kidatum, bement2.kategoria, bement2.ar)
-
-bement3 = jatek()
-bement3.nev = "Rocket League"
-bement3.kidatum = 2015
-bement3.kategoria = "AA"
-bement3.ar = 0
-print(bement3.nev, bement3.kidatum, bement3.kategoria, bement3.ar)
-
-
-jatek()'''
-
-
 class jatek:
     def __init__(self) -> None:
         super().__init__()
@@ -44,7 +9,7 @@ class jatek:
         self.ar: int = int(0)
 
 valt : bool = True
-fulloslist: List['str'] = list()
+fulloslist: List["jatek"] = list()
 
 while valt == True:
     bement4 = jatek()
@@ -52,10 +17,8 @@ while valt == True:
     bement4.kidatum = int(input("Kiadás éve: "))
     bement4.kategoria = str(input("Minősége: "))
     bement4.ar = int(input("A játék ára: "))
-    fulloslist.append(bement4.nev)
-    fulloslist.append(bement4.kidatum)
-    fulloslist.append(bement4.kategoria)
-    fulloslist.append(bement4.ar)
+    append4 = f"{bement4.nev} {bement4.kidatum} {bement4.kategoria} {bement4.ar}"
+    fulloslist.append(append4)
     new: str = str(input("Szeretne mégegy játékot felvinni?"))
     if new == "Nem":
         valt = False
@@ -66,34 +29,25 @@ bement.nev = str("Escape From Tarkov")
 bement.kidatum = int(2017)
 bement.kategoria = str("AAA")
 bement.ar = 35
-fulloslist.append(bement.nev)
-fulloslist.append(bement.kidatum)
-fulloslist.append(bement.kategoria)
-fulloslist.append(bement.ar)
+append = f"{bement.nev} {bement.kidatum} {bement.kategoria} {bement.ar}"
+fulloslist.append(append)
 
 bement2 = jatek()
 bement2.nev = "Valorant"
 bement2.kidatum = 2019
 bement2.kategoria = "AA"
 bement2.ar = 0
-fulloslist.append(bement2.nev)
-fulloslist.append(bement2.kidatum)
-fulloslist.append(bement2.kategoria)
-fulloslist.append(bement2.ar)
+append2 = f"{bement2.nev} {bement2.kidatum} {bement2.kategoria} {bement2.ar}"
+fulloslist.append(append2)
 
 bement3 = jatek()
 bement3.nev = "Rocket League"
 bement3.kidatum = 2015
 bement3.kategoria = "AA"
 bement3.ar = 0
-fulloslist.append(bement3.nev)
-fulloslist.append(bement3.kidatum)
-fulloslist.append(bement3.kategoria)
-fulloslist.append(bement3.ar)
+append3 = f"{bement3.nev} {bement3.kidatum} {bement3.kategoria} {bement3.ar}"
+fulloslist.append(append3)
 
 print(fulloslist)
-
-
-
 
 jatek()
