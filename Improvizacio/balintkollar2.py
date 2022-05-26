f9720e96d4a7a4cc2592023db91cc62d2052930f
Roland1520 <troznairoland@gmail.com>
2022-05-26 14:00:16 +0200

    push

--- Improvizacio/balintkollar2.py
+++ Improvizacio/balintkollar2.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Rendeles:
     def __init__(self):
         super().__init__()
@@ -19,7 +21,7 @@ termek1.szallitasikoltseg = 555
 termek1.kiszallitasiido = 30
 termek1.szin = "fekete"
 termek1.suly = 24
-print(termek1, "Szín = {a}; Súly = {b}".format(a=termek1.szin,b=termek1.szin))
+#print(termek1, "Szín = {a}; Súly = {b}".format(a=termek1.szin,b=termek1.szin))
 
 termek2 = Rendeles()
 termek2.termeknev = "Telefon"
@@ -28,7 +30,7 @@ termek2.szallitasikoltseg = 0
 termek2.kiszallitasiido = 30
 termek2.akummlator = 5000
 termek2.screen = 6.43
-print(termek2, "Akkumlátor = {a}; Screen = {b}".format(a=termek2.akummlator,b=termek2.screen))
+#print(termek2, "Akkumlátor = {a}; Screen = {b}".format(a=termek2.akummlator,b=termek2.screen))
 
 termek3 = Rendeles()
 termek3.termeknev = "Hangszoró"
@@ -37,7 +39,7 @@ termek3.szallitasikoltseg = 1638
 termek3.kiszallitasiido = 15
 termek3.vizallosag = True
 termek3.bluetoothversion = 5.0
-print(termek3, "Vízállóság = {a}; Bluetooth Version = {b}".format(a=termek3.vizallosag,b=termek3.bluetoothversion))
+#print(termek3, "Vízállóság = {a}; Bluetooth Version = {b}".format(a=termek3.vizallosag,b=termek3.bluetoothversion))
 
 termek4 = Rendeles()
 termek4.termeknev = "Drón"
@@ -46,7 +48,7 @@ termek4.szallitasikoltseg = 0
 termek4.kiszallitasiido = 60
 termek4.zoom = "50x"
 termek4.remotecontroldistance = 1200
-print(termek4, "Zoom méret = {a}; Remote control distance = {b}".format(a=termek4.zoom,b=termek4.remotecontroldistance))
+#print(termek4, "Zoom méret = {a}; Remote control distance = {b}".format(a=termek4.zoom,b=termek4.remotecontroldistance))
 
 termek5 = Rendeles()
 termek5.termeknev = "Okos Óra"
@@ -55,7 +57,24 @@ termek5.szallitasikoltseg = 0
 termek5.kiszallitasiido = 85
 termek5.type = "Lithium polymer"
 termek5.chargingtime = 2
-print(termek5, "Fajta = {a}; Töltési idő = {b}".format(a=termek5.type,b=termek5.chargingtime))
+#print(termek5, "Fajta = {a}; Töltési idő = {b}".format(a=termek5.type,b=termek5.chargingtime))
+
+Termeklista: List['termek1'] = list()
+Termeklista.append(termek1)
+Termeklista.append(termek2)
+Termeklista.append(termek3)
+Termeklista.append(termek4)
+Termeklista.append(termek5)
+print(Termeklista)
+
+Termeklista.remove(termek1) #Listából kivenni egy elemet .remove()
+print("Lista elemei:")
+for i in Termeklista:
+    print(i)
+
+
+
+
 
 
 
