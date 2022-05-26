0cd4c48feb002469d0cf9394e18a10d43cec3b71
HBoldi777 <71324114+HBoldi777@users.noreply.github.com>
2022-05-26 13:18:20 +0200

    asd

---
+++ Improvizacio/mandliivett2.py
@@ -0,0 +1,51 @@
+# 2het-2honap
+# ar szallitasikoltseg ido termek neve egykettulajdonsag
+
+class laptop:
+    def __init__(self):
+        self.ar = 69186
+        self.szallitasikoltseg = 0
+        self.szallitasiido = 7
+        self.szin = "szürke"
+        self.ram = 4
+        self.rom = 128
+
+
+A = laptop
+
+A.ar = 110126
+A.szallitasikoltseg = 5722
+A.szallitasiido = 7
+A.szin = "ezüst"
+A.ram = 16
+A.rom = 1024
+
+
+B = laptop
+
+B.ar = 73077
+B.szallitasikoltseg = 5954
+B.szallitasiido = 15
+B.szin = "fekete"
+B.ram = 4
+B.rom = 128
+
+
+C = laptop
+
+C.ar = 76943
+C.szallitasikoltseg = 5954
+C.szallitasiido = 15
+C.szin = "fekete"
+C.ram = 4
+C.rom = 640
+
+
+D = laptop
+
+D.ar = 163204
+D.szallitasikoltseg = 0
+D.szallitasiido = 7
+D.szin = "fekete"
+D.ram = 16
+D.rom = 512
