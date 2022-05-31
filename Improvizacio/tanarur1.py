5a43107c20e39310af47c368be6743ff941dd6b0
tuskeb <tuskebganz@gmail.com>
2022-05-31 12:34:03 +0200

    Auto

---
+++ Improvizacio/tanarur1.py
@@ -0,0 +1,59 @@
+from typing import List
+
+class Auto:
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.szin: str = ""
+        self.marka: str = ""
+        self.automatavaltos: bool = False
+        self.ar = 0
+
+    def __str__(self) -> str:
+        return self.szin + "\n" + self.marka + "\n" + str(self.automatavaltos) + "\n" + str(self.ar)
+
+
+s = Auto()
+s.szin = "Kék"
+s.marka = "Skoda"
+s.automatavaltos = False
+s.ar = 1000000
+
+b = Auto()
+b.szin = "Fekete"
+b.marka = "BMV"
+b.automatavaltos = True
+b.ar = 5000000
+
+z = Auto()
+z.szin = "Piros"
+z.ar = 150000
+z.marka = "Suzuki"
+z.automatavaltos = False
+
+
+l: List['Auto'] = list()
+
+l.append(s)
+l.append(b)
+l.append(z)
+
+b.ar = 49999999
+
+# print(s)
+# print(b)
+# print(z)
+
+a = Auto()
+a.szin = input("Kérem az autó színét: ")
+a.marka = input("Kérem az autó márkáját: ")
+a.ar = int(input("Kérem az autó árát: "))
+be: str = input("Automata váltós? (I/N): ")
+if be == "I":
+    a.automatavaltos = True
+else:
+    a.automatavaltos = False
+l.append(a)
+print("-------")
+for i in l:
+    print(i)
