853fd09b29454a58f7d78a112acf81586491fc4b
Roland1520 <troznairoland@gmail.com>
2022-05-31 14:17:56 +0200

    push1

--- Improvizacio/troznairoland2.py
+++ Improvizacio/troznairoland2.py
@@ -1,3 +1,4 @@
+import os
 from typing import List
 
 class focilabda:
@@ -10,37 +11,77 @@ class focilabda:
         self.hasznalat: str = ""
         self.hasznose: bool = False
 
-lista: List['focilabda'] = list()
+def strtobool(value: str) -> bool:
+    if value.upper() == "I" or value.upper() == "Y" or value.upper() == "TRUE":
+        return True
+    if value.upper() == "N" or value.upper() == "FALSE":
+        return False
+    return None
 
-focilabda1 = focilabda()
-focilabda1.szin = "fehér-fekete"
-focilabda1.meret = 5
-focilabda1.ar = 8000
-focilabda1.hasznalat = "kezdő"
-focilabda1.hasznose = True
-lista.append(focilabda1)
+def boolbeolvasas(prompt: str) -> bool:
+    while True:
+        be: str = input(prompt + "(I vagy N): ")
+        if be.upper() == "I" or be.upper() == "Y":
+            return True
+        if be.upper() == "N":
+            return False
 
-focilabda2 = focilabda()
-focilabda2.szin = "fehér-kék"
-focilabda2.meret = 5
-focilabda2.ar = 15000
-focilabda2.hasznalat = "haladó"
-focilabda2.hasznose = True
-lista.append(focilabda2)
+def meretbeolvas(prompt: str, min: int = 1, max: int = 5) -> int:
+    while True:
+        be: str = input(prompt + " (" + str(min) + " - " + str(max) + "): ")
+        try:
+            i: int = int(be)
+            if i >= min and i<=max:
+                return i
+        except:
+            pass
 
-focilabda3 = focilabda()
-focilabda3.szin = "sárga-fehér"
-focilabda3.meret = 5
-focilabda3.ar = 12000
-focilabda3.hasznalat = "haladó"
-focilabda3.hasznose = False
-lista.append(focilabda3)
+def arbeolvas(prompt: str, min: int = 1, max: int = 1000000) -> int:
+    while True:
+        be: str = input(prompt)
+        try:
+            i: int = int(be)
+            if i >= min and i<=max:
+                return i
+        except:
+            pass
 
-focilabda4 = focilabda()
-focilabda4.szin = input("A labda színe:")
-focilabda4.meret = input("A labda mérete:")
-focilabda4.ar = input("A labda ára forintban:")
-focilabda4.hasznalat = input("Megfelelő csoport:")
-focilabda4.hasznose = input("Hasznos-e a labda:")
-lista.append(focilabda4)
-print(lista)
+l: List['focilabda'] = list()
+
+fn = "troznairoland2.txt"
+
+fr = open(fn, mode="r", encoding="utf-8")
+sorok = fr.read().strip().split("\n")
+i: int = 0
+while i < len(sorok):
+    a = focilabda()
+    a.szin = sorok[i]
+    i += 1
+    a.meret = int(sorok[i])
+    i += 1
+    a.ar = int(sorok[i])
+    i += 1
+    a.hasznalat = sorok[i]
+    i += 1
+    a.hasznose = strtobool(sorok[i])
+    i += 1
+    l.append(a)
+fr.close()
+
+while boolbeolvasas("Szeretne-e focilabdát felvinni a billentyűzettel?: "):
+    focilabda4 = focilabda()
+    focilabda4.szin = input("A labda színe: ")
+    focilabda4.meret = meretbeolvas("Kérem a labda méretét: ")
+    focilabda4.ar = arbeolvas("A labda ára forintban: ")
+    focilabda4.hasznalat = input("Megfelelő csoport: ")
+    focilabda4.hasznose = boolbeolvasas("Hasznos-e a labda? ")
+    l.append(focilabda4)
+
+for i in l:
+    print(i)
+
+os.remove(fn)
+f = open(fn, mode="w", encoding="utf-8")
+for i in l:
+    f.write(i.__str__() + "\n")
+f.close()
