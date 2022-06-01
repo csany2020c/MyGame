4e6c1071886d8e8bae4d8efe9a241065da5ef5fb
Rigó <rdonat123@gmail.com>
2022-06-01 22:00:06 +0200

    hf gyak

--- Improvizacio/rigodonat2.py
+++ Improvizacio/rigodonat2.py
@@ -18,6 +18,32 @@ class napszemuveg:
     def __str__(self) -> str:
         return "Név = {a}; Lencseszín = {b}; Napra sőtétedik e = {c}; Ár = {d}; Szállítási idő = {e}; Szállítási költség = {f}".format(a=self.nev, b=self.lencseszin, c=self.naprasotetedike, d=self.ar, e=self.szallitasiido, f=self.szallitasikoltseg)
 
+
+def strtobool(value: str) -> bool:
+    if value.upper() == "I" or value.upper() == "Y" or value.upper() == "TRUE":
+        return True
+    if value.upper() == "N" or value.upper() == "FALSE":
+        return False
+    return None
+def boolbeolvas(prompt: str) -> bool:
+    while True:
+        be: str = input(prompt + " (I/N): ")
+        if be.upper() == "I" or be.upper() == "Y":
+            return True
+        if be.upper() == "N":
+            return False
+def intbeolvas(prompt: str, min: int = 0, max: int = 100) -> int:
+    while True:
+        be: str = input(prompt + " (" + str(min) + " - " + str(max) + "): ")
+        try:
+            i: int = int(be)
+            if i >= min and i <= max:
+                return i
+            else:
+                print("Hibás érték! Nem az intervallumba tartozó szám!")
+        except:
+            print("Hibás érték! Számot kérek!")
+
 MyNapszemuveg = napszemuveg()
 MyNapszemuveg.nev = "Női Nagy Szemüveg"
 MyNapszemuveg.lencseszin = "Kék"
@@ -53,6 +79,9 @@ lista: List[napszemuveg] = list()
 lista.append(MyNapszemuveg)
 lista.append(MyNapszemuveg2)
 lista.append(MyNapszemuveg3)
+
+
+MyNapszemuveg2.ar = 1500
 
 for i in lista:
     print(i)
