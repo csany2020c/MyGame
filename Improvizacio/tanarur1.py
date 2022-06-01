68454d989d065f5f85e1c26b85c19e4ae43a3339
tuskeb <tuskebganz@gmail.com>
2022-06-01 13:25:14 +0200

    fájlbaírás, olvasás

--- Improvizacio/tanarur1.py
+++ Improvizacio/tanarur1.py
@@ -13,15 +13,12 @@ class Auto:
     def __str__(self) -> str:
         return self.szin + "\n" + self.marka + "\n" + str(self.automatavaltos) + "\n" + str(self.ar)
 
-
 def strtobool(value: str) -> bool:
     if value.upper() == "I" or value.upper() == "Y" or value.upper() == "TRUE":
         return True
     if value.upper() == "N" or value.upper() == "FALSE":
         return False
     return None
-
-
 def boolbeolvas(prompt: str) -> bool:
     while True:
         be: str = input(prompt + " (I/N): ")
@@ -29,51 +26,59 @@ def boolbeolvas(prompt: str) -> bool:
             return True
         if be.upper() == "N":
             return False
-
-
 def intbeolvas(prompt: str, min: int = 0, max: int = 100) -> int:
     while True:
         be: str = input(prompt + " (" + str(min) + " - " + str(max) + "): ")
         try:
             i: int = int(be)
-            if i >= min and i<=max:
+            if i >= min and i <= max:
                 return i
+            else:
+                print("Hibás érték! Nem az intervallumba tartozó szám!")
         except:
-            pass
+            print("Hibás érték! Számot kérek!")
+s: Auto = Auto()
+s.szin = "Kék"
+s.marka = "Skoda"
+s.automatavaltos = False
+s.ar = 1000000
 
+b: Auto = Auto()
+b.szin = "Fekete"
+b.marka = "BMV"
+b.automatavaltos = True
+b.ar = 5000000
 
-# s = Auto()
-# s.szin = "Kék"
-# s.marka = "Skoda"
-# s.automatavaltos = False
-# s.ar = 1000000
-#
-# b = Auto()
-# b.szin = "Fekete"
-# b.marka = "BMV"
-# b.automatavaltos = True
-# b.ar = 5000000
-#
-# z = Auto()
-# z.szin = "Piros"
-# z.ar = 150000
-# z.marka = "Suzuki"
-# z.automatavaltos = False
+z: Auto = Auto()
+z.szin = "Piros"
+z.ar = 150000
+z.marka = "Suzuki"
+z.automatavaltos = False
+
 
 
 l: List['Auto'] = list()
 
-# l.append(s)
-# l.append(b)
-# l.append(z)
-#
-# b.ar = 49999999
+l.append(s)
+l.append(b)
+l.append(z)
+
+b.ar = 4999999
+print(b)
+
+for i in l:
+    print(i)
 
 
+a = Auto()
+a.szin = input("Kérem az autó színét: ")
+a.marka = input("Kérem az autó márkáját: ")
+a.ar = intbeolvas("Kérem az autó árát: ", 20000, 999999999)
+a.automatavaltos = boolbeolvas("Automataváltós")
+l.append(a)
 
-# print(s)
-# print(b)
-# print(z)
+exit()
+
 fn = "tanarur1.txt"
 
 fr = open(fn, mode="r", encoding="utf-8")
