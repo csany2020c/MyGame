c0e8c1a8729d0d61a7d27fb5c50a493ded7b1e77
Rotyka5tud10 <simon.roland.daniel@gmail.com>
2022-06-02 08:01:36 +0200

    asdf

--- Improvizacio/rigodonat2.py
+++ Improvizacio/rigodonat2.py
@@ -19,31 +19,6 @@ class napszemuveg:
         return "Név = {a}; Lencseszín = {b}; Napra sőtétedik e = {c}; Ár = {d}; Szállítási idő = {e}; Szállítási költség = {f}".format(a=self.nev, b=self.lencseszin, c=self.naprasotetedike, d=self.ar, e=self.szallitasiido, f=self.szallitasikoltseg)
 
 
-def strtobool(value: str) -> bool:
-    if value.upper() == "I" or value.upper() == "Y" or value.upper() == "TRUE":
-        return True
-    if value.upper() == "N" or value.upper() == "FALSE":
-        return False
-    return None
-def boolbeolvas(prompt: str) -> bool:
-    while True:
-        be: str = input(prompt + " (I/N): ")
-        if be.upper() == "I" or be.upper() == "Y":
-            return True
-        if be.upper() == "N":
-            return False
-def intbeolvas(prompt: str, min: int = 0, max: int = 100) -> int:
-    while True:
-        be: str = input(prompt + " (" + str(min) + " - " + str(max) + "): ")
-        try:
-            i: int = int(be)
-            if i >= min and i <= max:
-                return i
-            else:
-                print("Hibás érték! Nem az intervallumba tartozó szám!")
-        except:
-            print("Hibás érték! Számot kérek!")
-
 MyNapszemuveg = napszemuveg()
 MyNapszemuveg.nev = "Női Nagy Szemüveg"
 MyNapszemuveg.lencseszin = "Kék"
