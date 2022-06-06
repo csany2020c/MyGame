19270fa3cd81c4b0000f296df33db73d0951fe01
kalamarrajmund <rajmundka1@gmail.com>
2022-06-06 19:10:42 +0200

    hazixd

---
+++ Improvizacio/krhazi.py
@@ -0,0 +1,57 @@
+from typing import List
+import random
+from random import randint
+
+# print("Első feladat")
+# szam1:int = int(input("Kérek egy számot:"))
+# szam2:int = int(input("Kérek egy számot:"))
+# szam3:int = int(input("Kérek egy számot:"))
+# szamok = [szam1, szam2, szam3]
+# print("legnagyobb szam:", max(szamok))
+#
+# print("Második feladat")
+# listaxd = [1,2,3,6,7,10,9,5]
+#
+# a = 0
+# for i in listaxd:
+#     a += i
+#     print(a)
+#
+# if a != 0:
+#     a /= len(listaxd)
+#     print(a)
+#
+# for i in listaxd:
+#     if i < a:
+#         print(i)
+#
+# print("Harmadik feladat (nem értemxd)")
+# print("Negyedik feladat")
+#
+# a = 21
+# szam4: int = int(input("Kérek egy számot:"))
+# while szam4 % 21 != 0:
+#     szam4: int = int(input("Kérek egy számot:"))
+# number = random.randint(0,127)
+# print(number)
+#
+# szam5: int = int(input("Tippelj egy számot:"))
+# while szam5 != number:
+#     szam5: int = int(input("Tippelj egy számot:"))
+#     if szam5 > number:
+#         print("A kitalált szám kisebb!")
+#
+#     else:
+#         print("A kitalált szám nagyobb!")
+
+#3.feladat
+# lista = []
+# for i in range(1,91):
+#     lista.append(i)
+# print(lista)
+#
+# for x in lista:
+#     if x > 0:
+#         x = random.randint(1, 90)
+#         print(x)
+#
