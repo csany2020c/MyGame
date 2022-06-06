2251f866c61c628431e442253e80777f4ae113e3
Laszlo9 <71323987+Laszlo9@users.noreply.github.com>
2022-06-06 19:49:46 +0200

    FL hf

--- Hazi/faterlaszlo.py
+++ Hazi/faterlaszlo.py
@@ -1,37 +1,37 @@
 from typing import List
 from random import randint
-# def elsofeladat() -> int:
-#     elso: int = int(input("írjon be egy számot: "))
-#     masodik: int = int(input("írjon be egy számot: "))
-#     harmadik: int = int(input("írjon be egy számot: "))
-#     a = elso
-#     for i in range(2):
-#         if a < masodik:
-#             a = masodik
-#         if a < harmadik:
-#             a = harmadik
-#     print(a)
-#
-# elsofeladat()
 
-# def masodikfeladat() -> int:
-#     lista = [5, 7, 5, 4, 15, 5, 20, 45]
-#     a: int = 0
-#     for i in lista:
-#         a += i
-#     a = a / 8
-#     for i in lista:
-#         if a > i:
-#             print(i)
-#
-# masodikfeladat()
+def elsofeladat() -> int:
+    elso: int = int(input("írjon be egy számot: "))
+    masodik: int = int(input("írjon be egy számot: "))
+    harmadik: int = int(input("írjon be egy számot: "))
+    a = elso
+    for i in range(2):
+        if a < masodik:
+            a = masodik
+        if a < harmadik:
+            a = harmadik
+    print(a)
+
+elsofeladat()
+
+def masodikfeladat() -> int:
+    lista = [5, 7, 5, 4, 15, 5, 20, 45]
+    a: int = 0
+    for i in lista:
+        a += i
+    a = a / 8
+    for i in lista:
+        if a > i:
+            print(i)
+
+masodikfeladat()
 
 
 def harmadikfeladat() -> int:
     lista: List['int'] = list()
-    for i in range(10):
-        a = randint(1, 90)
-        lista.append(a)
+    for i in range(1, 91):
+        lista.append(i)
     # print(lista)
 
     for i in range(200):
@@ -46,29 +46,28 @@ def harmadikfeladat() -> int:
     for i in range(0, 4):
         print(lista[i])
 
-
 harmadikfeladat()
 
-# def negyedik():
-#     while True:
-#         a: int = int(input("Írjon be egy számot: "))
-#         if a % 21 == 0:
-#             return False
-#         else:
-#             pass
-#
-# negyedik()
+def negyedik():
+    while True:
+        a: int = int(input("Írjon be egy számot: "))
+        if a % 21 == 0:
+            return False
+        else:
+            pass
 
-# def szamkitalalos() -> int:
-#     a = randint(0, 127)
-#     # print(a)
-#     while True:
-#         bejovo: int = int(input("Írjon be egy számot: "))
-#         if bejovo == a:
-#             return False
-#         if bejovo > a:
-#             print("Kisebb számot írjon be!")
-#         if bejovo < a:
-#             print("Nagyobb számot írjon be!")
-#
-# szamkitalalos()
+negyedik()
+
+def szamkitalalos() -> int:
+    a = randint(0, 127)
+    # print(a)
+    while True:
+        bejovo: int = int(input("Írjon be egy számot: "))
+        if bejovo == a:
+            return False
+        if bejovo > a:
+            print("Kisebb számot írjon be!")
+        if bejovo < a:
+            print("Nagyobb számot írjon be!")
+
+szamkitalalos()
