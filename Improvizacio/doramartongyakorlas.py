6564f9a63d802437a0a585585aeba300b3a8f020
Win10 <doramarton20@gmail.com>
2022-06-08 20:36:20 +0200

    asd

--- Improvizacio/doramartongyakorlas.py
+++ Improvizacio/doramartongyakorlas.py
@@ -1,17 +1,54 @@
-from typing import List
+from typing import *
 from random import randint
 
+def elso():
+    szam1 = int(input())
+    szam2 = int(input())
+    szam3 = int(input())
+    print("legnagyobb szám: {a}".format(a=max(szam1, szam2, szam3)))
 
-def kutya(a: int, b: int) -> List['int']:
-    eredmenycucc: List['int'] = list()
-    for i in range(b):
-        cicu = a * (i + 1)
-        eredmenycucc.append(cicu)
-    return eredmenycucc
+#elso()
 
+def masodik():
+    lista: list = [1, 2, 3, 4, 5, 6, 7, 8]
+    osszeg: int = 0
+    szamok: int = 0
+    for i in lista:
+        osszeg += i
+        szamok = i
+    atlag: int = osszeg / szamok
+    for i in lista:
+        if i < atlag:
+            print(i)
+#masodik()
 
-# print(kutya(int(input()), int(input())))
+#def harmadik():
+    #???
 
-def vereb():
-    kjdshvbqwkra: List['str'] = list()
+def negyedik():
     while True:
+        szam = int(input())
+        if szam % 21:
+            print("butavagy")
+            pass
+        else:
+            print("jej")
+            break
+
+#negyedik()
+
+def ototdik():
+    szam1 = int(randint(0, 127))
+    while True:
+        szam2 = int(input())
+        if szam1 > szam2:
+            print("ez a szám túl kicsi")
+            pass
+        else:
+            print("ez a szám túl nagy")
+            pass
+        if szam1 == szam2:
+            print("eltaláltad!")
+            break
+
+ototdik()
